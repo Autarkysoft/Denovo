5be52ddda21419f0c41982747321bfbd279760cf
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-01-12 13:16:03 +0330

    Add base16 + tests

--- Src/Autarkysoft.Bitcoin/Encoders/Base16.cs
+++ Src/Autarkysoft.Bitcoin/Encoders/Base16.cs
@@ -4,14 +4,96 @@
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
 using System;
+using System.Linq;
 
 namespace Autarkysoft.Bitcoin.Encoders
 {
+    /// <summary>
+    /// Base-16 or hexadecimal string encoder using only lower case letters.
+    /// </summary>
     public class Base16
     {
-        internal static string Encode(byte[] data)
+        /// <summary>
+        /// The prefix that could be appended to the beginning of Base-16 strings.
+        /// </summary>
+        public const string Prefix = "0x";
+        private const string Base16Chars = "0123456789abcdef";
+
+
+        /// <summary>
+        /// Checks to see if a given string is a valid base-16 encoded string.
+        /// </summary>
+        /// <param name="hexToCheck">Hex string to check.</param>
+        /// <returns>True if valid, fale if otherwise.</returns>
+        public static bool IsValid(string hexToCheck)
         {
-            throw new NotImplementedException();
+            if (hexToCheck is null || hexToCheck.Length % 2 != 0)
+            {
+                return false;
+            }
+            if (hexToCheck.StartsWith(Prefix))
+            {
+                hexToCheck = hexToCheck.Substring(2);
+            }
+
+            return hexToCheck.All(c => Base16Chars.Contains(char.ToLower(c)));
         }
+
+
+        /// <summary>
+        /// Converts the given base-16 encoded string to its byte array representation.
+        /// </summary>
+        /// <exception cref="ArgumentException"/>
+        /// <param name="hex">Hex to convert.</param>
+        /// <returns>An array of bytes</returns>
+        public static byte[] Decode(string hex)
+        {
+            if (!IsValid(hex))
+                throw new ArgumentException($"Input is not a valid hex. <{hex}>");
+
+
+            if (hex.StartsWith(Prefix))
+            {
+                hex = hex.Substring(2);
+            }
+
+            byte[] ba = new byte[hex.Length / 2];
+            for (int i = 0; i < ba.Length; i++)
+            {
+                int hi = hex[i * 2] - 65;
+                hi = hi + 10 + ((hi >> 31) & 7);
+
+                int lo = hex[i * 2 + 1] - 65;
+                lo = lo + 10 + ((lo >> 31) & 7) & 0x0f;
+
+                ba[i] = (byte)(lo | hi << 4);
+            }
+            return ba;
+        }
+
+        /// <summary>
+        /// Converts the given byte array to base-16 (Hexadecimal) encoded string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <param name="data">The array of bytes to convert.</param>
+        /// <returns>Base-16 (Hexadecimal) encoded string.</returns>
+        public static string Encode(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data), "Byte array can not be null.");
+
+
+            char[] ca = new char[data.Length * 2];
+            int b;
+            for (int i = 0; i < data.Length; i++)
+            {
+                b = data[i] >> 4;
+                ca[i * 2] = (char)(87 + b + (((b - 10) >> 31) & -39));
+                b = data[i] & 0xF;
+                ca[i * 2 + 1] = (char)(87 + b + (((b - 10) >> 31) & -39));
+            }
+            return new string(ca);
+        }
+
     }
 }
---
+++ Src/Tests/Bitcoin/Encoders/Base16Tests.cs
@@ -0,0 +1,95 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Encoders;
+using System;
+using System.Text;
+using Xunit;
+
+namespace Tests.Bitcoin.Encoders
+{
+    public class Base16Tests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("0x")]
+        [InlineData("01")]
+        [InlineData("12abcf")]
+        [InlineData("12ABCF")]
+        [InlineData("0x12abcf")]
+        public void IsValidTest(string hex)
+        {
+            Assert.True(Base16.IsValid(hex));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("3")]
+        [InlineData("abc")]
+        [InlineData("ABC")]
+        [InlineData("abcg")]
+        [InlineData("0x1")]
+        public void IsValid_FalseTest(string hex)
+        {
+            Assert.False(Base16.IsValid(hex));
+        }
+
+        [Theory]
+        [InlineData(new byte[] { }, "")]
+        [InlineData(new byte[] { 0 }, "00")]
+        [InlineData(new byte[] { 0, 0 }, "0000")]
+        [InlineData(new byte[] { 16, 42, 255 }, "102aff")]
+        [InlineData(new byte[] { 16, 42, 255 }, "0x102aff")]
+        [InlineData(new byte[] { 0, 0, 2 }, "000002")]
+        public void DecodeTest(byte[] expected, string hex)
+        {
+            byte[] actual = Base16.Decode(hex);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("x")]
+        public void Decode_ExceptionTest(string hex)
+        {
+            Assert.Throws<ArgumentException>(() => Base16.Decode(hex));
+        }
+
+        [Theory]
+        [InlineData(new byte[] { }, "")]
+        [InlineData(new byte[] { 0 }, "00")]
+        [InlineData(new byte[] { 0, 0 }, "0000")]
+        [InlineData(new byte[] { 16, 42, 255 }, "102aff")]
+        [InlineData(new byte[] { 0, 0, 2 }, "000002")]
+        public void EncodeTest(byte[] ba, string expected)
+        {
+            string actual = Base16.Encode(ba);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Encode_AllCharsTest()
+        {
+            byte[] allHexChars = new byte[256];
+            StringBuilder sb = new StringBuilder(256 * 2);
+            for (int i = 0; i < 256; i++)
+            {
+                allHexChars[i] = (byte)i;
+                sb.Append($"{allHexChars[i]:x2}");
+            }
+
+            Assert.Equal(sb.ToString(), Base16.Encode(allHexChars));
+        }
+
+        [Fact]
+        public void Encode_ExceptionTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => Base16.Encode(null));
+        }
+
+    }
+}
