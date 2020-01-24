1ac67aeabf9532b1de8e265fe3355705371d826f
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-01-24 09:25:09 +0330

    Add 2 more OP tests

---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/SimpleRunableOpsTests.cs
@@ -0,0 +1,31 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+using System;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Scripts.Operations
+{
+    public class SimpleRunableOpsTests
+    {
+        private class MockOp : SimpleRunableOps
+        {
+            public override OP OpValue => throw new NotImplementedException();
+        }
+
+
+        [Fact]
+        public void RunTest()
+        {
+            MockOp op = new MockOp();
+            bool b = op.Run(null, out string error);
+
+            Assert.True(b);
+            Assert.Null(error);
+        }
+    }
+}
---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/SizeOpTests.cs
@@ -0,0 +1,39 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Scripts.Operations
+{
+    public class SizeOpTests
+    {
+        [Fact]
+        public void RunTest()
+        {
+            MockOpData data = new MockOpData(FuncCallName.Peek, FuncCallName.Push)
+            {
+                _itemCount = 1,
+                peekData = new byte[][] { OpTestCaseHelper.b1 },
+                pushData = new byte[][] { OpTestCaseHelper.num3 },
+            };
+
+            OpTestCaseHelper.RunTest<SizeOp>(data, OP.SIZE);
+        }
+
+
+        [Fact]
+        public void Run_FailTest()
+        {
+            MockOpData data = new MockOpData(FuncCallName.Peek)
+            {
+                _itemCount = 0,
+            };
+
+            OpTestCaseHelper.RunFailTest<SizeOp>(data, Err.OpNotEnoughItems);
+        }
+    }
+}
