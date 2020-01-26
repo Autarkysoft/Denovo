95c7c7aaa3213892e0e6de81ea663df7b4af4bee
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-01-26 08:20:54 +0330

    Add 2 more OP tests

---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/EqualOpTests.cs
@@ -0,0 +1,99 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Scripts.Operations
+{
+    public class EqualOpTests
+    {
+        [Theory]
+        [InlineData(new byte[0] { }, new byte[] { 1 })]
+        [InlineData(new byte[] { 1 }, new byte[] { 2 })]
+        [InlineData(new byte[] { 1 }, new byte[] { 1, 2 })]
+        public void Run_NotEqual_Test(byte[] ba1, byte[] ba2)
+        {
+            MockOpData data = new MockOpData(FuncCallName.Pop, FuncCallName.Pop, FuncCallName.Push)
+            {
+                _itemCount = 2,
+                popData = new byte[][] { ba1, ba2 },
+                pushData = new byte[][] { OpTestCaseHelper.FalseBytes },
+            };
+
+            OpTestCaseHelper.RunTest<EqualOp>(data, OP.EQUAL);
+        }
+
+        [Theory]
+        [InlineData(new byte[0] { }, new byte[0] { })]
+        [InlineData(new byte[] { 1 }, new byte[] { 1 })]
+        [InlineData(new byte[] { 1, 2 }, new byte[] { 1, 2 })]
+        public void Run_Equal_Test(byte[] ba1, byte[] ba2)
+        {
+            MockOpData data = new MockOpData(FuncCallName.Pop, FuncCallName.Pop, FuncCallName.Push)
+            {
+                _itemCount = 2,
+                popData = new byte[][] { ba1, ba2 },
+                pushData = new byte[][] { OpTestCaseHelper.TrueBytes },
+            };
+
+            OpTestCaseHelper.RunTest<EqualOp>(data, OP.EQUAL);
+        }
+
+        [Fact]
+        public void Run_FailTest()
+        {
+            MockOpData data = new MockOpData(FuncCallName.Pop)
+            {
+                _itemCount = 1,
+            };
+
+            OpTestCaseHelper.RunFailTest<EqualOp>(data, Err.OpNotEnoughItems);
+        }
+
+
+        [Theory]
+        [InlineData(new byte[0] { }, new byte[] { 1 })]
+        [InlineData(new byte[] { 1 }, new byte[] { 2 })]
+        [InlineData(new byte[] { 1 }, new byte[] { 1, 2 })]
+        public void Run_NotEqualVerify_Test(byte[] ba1, byte[] ba2)
+        {
+            MockOpData data = new MockOpData(FuncCallName.Pop, FuncCallName.Pop)
+            {
+                _itemCount = 2,
+                popData = new byte[][] { ba1, ba2 },
+            };
+
+            OpTestCaseHelper.RunFailTest<EqualVerifyOp>(data, "Top two stack items are not equal.");
+        }
+
+        [Theory]
+        [InlineData(new byte[0] { }, new byte[0] { })]
+        [InlineData(new byte[] { 1 }, new byte[] { 1 })]
+        [InlineData(new byte[] { 1, 2 }, new byte[] { 1, 2 })]
+        public void Run_EqualVerify_Test(byte[] ba1, byte[] ba2)
+        {
+            MockOpData data = new MockOpData(FuncCallName.Pop, FuncCallName.Pop)
+            {
+                _itemCount = 2,
+                popData = new byte[][] { ba1, ba2 },
+            };
+
+            OpTestCaseHelper.RunTest<EqualVerifyOp>(data, OP.EqualVerify);
+        }
+
+        [Fact]
+        public void Run_VerifyFailTest()
+        {
+            MockOpData data = new MockOpData(FuncCallName.Pop)
+            {
+                _itemCount = 1,
+            };
+
+            OpTestCaseHelper.RunFailTest<EqualVerifyOp>(data, Err.OpNotEnoughItems);
+        }
+    }
+}
---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/NotRunableOpsTests.cs
@@ -0,0 +1,32 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Scripts.Operations
+{
+    public class NotRunableOpsTests
+    {
+        public static IEnumerable<object[]> GetRunCases()
+        {
+            yield return new object[] { new ReservedOp(), OP.Reserved.ToString() };
+            yield return new object[] { new VEROp(), OP.VER.ToString() };
+            yield return new object[] { new Reserved1Op(), OP.Reserved1.ToString() };
+            yield return new object[] { new Reserved2Op(), OP.Reserved2.ToString() };
+            yield return new object[] { new ReturnOp(), OP.RETURN.ToString() };
+        }
+        [Theory]
+        [MemberData(nameof(GetRunCases))]
+        public void RunTest(NotRunableOps op, string name)
+        {
+            bool b = op.Run(null, out string error);
+            Assert.False(b);
+            Assert.Equal($"Can not run an OP_{name} operation.", error);
+        }
+    }
+}
