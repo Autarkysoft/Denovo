b4a234c47e1602bdc46b667b99d77666d972caed
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-01-28 10:59:37 +0330

    Add BIP-32 path + tests

---
+++ Src/Autarkysoft.Bitcoin/ImprovementProposals/BIP0032Path.cs
@@ -0,0 +1,116 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Autarkysoft.Bitcoin.ImprovementProposals
+{
+    /// <summary>
+    /// Represents the derivation paths used in <see cref="BIP0032"/> deterministic keys.
+    /// </summary>
+    public class BIP0032Path
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="BIP0032Path"/> using the given indexes.
+        /// </summary>
+        /// <param name="ui">An array of indexes (use null, to represents the master)</param>
+        public BIP0032Path(params uint[] ui)
+        {
+            Indexes = ui ?? new uint[0];
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="BIP0032Path"/> from the given string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="FormatException"/>
+        /// <param name="path">
+        /// The path string (must start with letter 'm', each index separated with '/' and hardened indexes must use ''' or 'h')
+        /// </param>
+        public BIP0032Path(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentNullException("Path can not be null or empty.");
+            }
+            path = path.ToLower();
+            if (!path.StartsWith("m"))
+            {
+                throw new FormatException("Path should start with letter \"m\".");
+            }
+
+            string[] parts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length > byte.MaxValue)
+            {
+                throw new FormatException("Depth can not be bigger than 1 byte.");
+            }
+
+            Indexes = new uint[0];
+
+            if (parts.Length > 1)
+            {
+                Indexes = new uint[parts.Length - 1];
+                for (int i = 1; i < parts.Length; i++)
+                {
+                    // remove any extra whitespace.
+                    string num = parts[i].Replace(" ", "");
+                    if (hardenedChars.Any(x => num.Contains(x)))
+                    {
+                        num = num[0..^1];
+                        if (hardenedChars.Any(x => num.Contains(x)))
+                        {
+                            throw new FormatException("Input contains more than one indicator for hardened key.");
+                        }
+                        Indexes[i - 1] = 0x80000000;
+                    }
+
+                    if (!uint.TryParse(num, out uint ui))
+                    {
+                        throw new FormatException($"Input ({num}) is not a valid positive number.");
+                    }
+                    if ((ui & 0x80000000) != 0)
+                    {
+                        throw new FormatException("Index is too big.");
+                    }
+                    Indexes[i - 1] += ui;
+                }
+            }
+        }
+
+
+
+        private readonly char[] hardenedChars = { '\'', 'h' };
+        /// <summary>
+        /// An array of <see cref="BIP0032Path"/> indexes.
+        /// </summary>
+        public uint[] Indexes { get; private set; }
+
+
+
+        /// <summary>
+        /// Converts this instance to its string representation.
+        /// </summary>
+        /// <returns>A string starting with 'm' indicating master followed by each index</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder("m");
+            foreach (var item in Indexes)
+            {
+                if ((item & 0x80000000) != 0)
+                {
+                    sb.Append($"/{item - 0x80000000}{hardenedChars[0]}");
+                }
+                else
+                {
+                    sb.Append($"/{item}");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Src/Tests/Bitcoin/ImprovementProposals/BIP0032PathTests.cs
@@ -0,0 +1,99 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.ImprovementProposals;
+using System;
+using Xunit;
+
+namespace Tests.Bitcoin.ImprovementProposals
+{
+    public class BIP0032PathTests
+    {
+        private const uint Hard = 0x80000000;
+
+        [Fact]
+        public void Constructor_FromUIntsTest()
+        {
+            uint[] uints = { 0x80000000, 1, 0x80000000 + 2, 2 };
+            BIP0032Path path = new BIP0032Path(uints);
+            Assert.Equal(uints, path.Indexes);
+        }
+
+        [Fact]
+        public void Constructor_FromUInts_NullTest()
+        {
+            BIP0032Path path = new BIP0032Path();
+            Assert.Empty(path.Indexes);
+        }
+
+
+        [Theory]
+        [InlineData("m", new uint[0])]
+        [InlineData("m/", new uint[0])]
+        [InlineData("m/0", new uint[1] { 0 })]
+        [InlineData("m/5", new uint[1] { 5 })]
+        [InlineData("m/0'", new uint[1] { Hard })]
+        [InlineData("m/5'", new uint[1] { Hard + 5 })]
+        [InlineData("m/0'/1/2'/43", new uint[4] { Hard, 1, Hard + 2, 43 })]
+        // With extra spaces:
+        [InlineData("m/8 '/ 1/0 ' /12 ", new uint[4] { Hard + 8, 1, Hard, 12 })]
+        // Using 'h' and "H" to indicate hardened paths:
+        [InlineData("m/3h/2H/6", new uint[3] { Hard + 3, Hard + 2, 6 })]
+        // Empty item at the end (extra /) is ignored
+        [InlineData("m/5/", new uint[1] { 5 })]
+        public void Constructor_FromStringTest(string toUse, uint[] expected)
+        {
+            var path = new BIP0032Path(toUse);
+            Assert.Equal(expected, path.Indexes);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Constructor_FromString_NullExceptionTest(string path)
+        {
+            Assert.Throws<ArgumentNullException>(() => new BIP0032Path(path));
+        }
+
+        [Theory]
+        [InlineData("0'/1/2'/2", "Path should start with letter \"m\".")]
+        [InlineData("m/1/2'h", "Input contains more than one indicator for hardened key.")]
+        [InlineData("m/1/g", "Input (g) is not a valid positive number.")]
+        [InlineData("m/1/-1", "Input (-1) is not a valid positive number.")]
+        [InlineData("m/2147483648/2", "Index is too big.")]
+        public void Constructor_FromString_FormatExceptionTest(string path, string expErr)
+        {
+            Exception ex = Assert.Throws<FormatException>(() => new BIP0032Path(path));
+            Assert.Contains(expErr, ex.Message);
+        }
+
+        [Fact]
+        public void Constructor_FromString_DepthOverflowTest()
+        {
+            char[] temp = new char[511];
+            temp[0] = 'm';
+            for (int i = 1; i < temp.Length; i += 2)
+            {
+                temp[i] = '/';
+                temp[i + 1] = '1';
+            }
+            Exception ex = Assert.Throws<FormatException>(() => new BIP0032Path(new string(temp)));
+            Assert.Contains("Depth can not be bigger than 1 byte.", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(new uint[0], "m")]
+        [InlineData(new uint[] { 0 }, "m/0")]
+        [InlineData(new uint[] { 2 }, "m/2")]
+        [InlineData(new uint[] { Hard, 1, Hard + 2, 2 }, "m/0'/1/2'/2")]
+        public void ToStringTest(uint[] toUse, string expected)
+        {
+            var path = new BIP0032Path(toUse);
+            string actual = path.ToString();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
