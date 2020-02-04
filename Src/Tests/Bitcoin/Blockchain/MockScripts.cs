120e31c4a42890e6d2a61f95b4b19b0821eeb3df
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-02-04 11:29:40 +0330

    Add some mock scripts for tests

--- Src/Tests/Bitcoin/Blockchain/MockScripts.cs
+++ Src/Tests/Bitcoin/Blockchain/MockScripts.cs
@@ -7,17 +7,18 @@ using Autarkysoft.Bitcoin;
 using Autarkysoft.Bitcoin.Blockchain.Scripts;
 using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
 using System;
+using Xunit;
 
 namespace Tests.Bitcoin.Blockchain
 {
-    internal abstract class MockScriptBase : IScript
+    public abstract class MockScriptBase : IScript
     {
         public virtual bool IsWitness => throw new NotImplementedException();
         public virtual ScriptType ScriptType => throw new NotImplementedException();
-        public virtual IOperation[] OperationList 
-        { 
+        public virtual IOperation[] OperationList
+        {
             get => throw new NotImplementedException();
-            set => throw new NotImplementedException(); 
+            set => throw new NotImplementedException();
         }
 
         public virtual void Serialize(FastStream stream)
@@ -43,7 +44,7 @@ namespace Tests.Bitcoin.Blockchain
 
 
 
-    internal class MockSerializableScript : MockScriptBase
+    public class MockSerializableScript : MockScriptBase
     {
         public MockSerializableScript(byte[] serializedResult)
         {
@@ -66,5 +67,84 @@ namespace Tests.Bitcoin.Blockchain
         {
             return ba;
         }
+    }
+
+
+    public class MockSerializablePubScript : MockSerializableScript, IPubkeyScript
+    {
+        public MockSerializablePubScript(PubkeyScriptType typeResult, byte[] serializedResult) : base(serializedResult)
+        {
+            typeToReturn = typeResult;
+        }
+
+        public MockSerializablePubScript(byte[] serializedResult) : this(PubkeyScriptType.Unknown, serializedResult)
+        {
+        }
+
+        private readonly PubkeyScriptType typeToReturn;
+
+        public PubkeyScriptType GetPublicScriptType() => typeToReturn;
+    }
+
+
+    public class MockSerializableSigScript : MockSerializableScript, ISignatureScript
+    {
+        public MockSerializableSigScript(byte[] serializedResult) : base(serializedResult)
+        {
+        }
+    }
+
+
+    public class MockDeserializableScript : MockScriptBase
+    {
+        /// <summary>
+        /// Use this mock object to mock deserialization. It will check current index in stream and fails if it is not expected.
+        /// Also it can return true or false depending on whether error is null or not.
+        /// </summary>
+        /// <param name="streamIndex">Expected current stream index</param>
+        /// <param name="bytesToRead">Number of bytes to read (move stream index forward)</param>
+        /// <param name="errorToReturn">Custom error to return (null returns true, otherwise false)</param>
+        public MockDeserializableScript(int streamIndex, int bytesToRead, string errorToReturn = null)
+        {
+            expectedIndex = streamIndex;
+            retError = errorToReturn;
+            this.bytesToRead = bytesToRead;
+        }
+
+        private readonly int expectedIndex;
+        private readonly int bytesToRead;
+        private readonly string retError;
+
+        public override bool TryDeserialize(FastStreamReader stream, out string error)
+        {
+            int actualIndex = stream.GetCurrentIndex();
+            Assert.Equal(expectedIndex, actualIndex);
+
+            if (!stream.TryReadByteArray(bytesToRead, out _))
+            {
+                Assert.True(false, "Stream doesn't have enough bytes.");
+            }
+
+            error = retError;
+            return string.IsNullOrEmpty(retError);
+        }
+    }
+
+
+    public class MockDeserializablePubScript : MockDeserializableScript, IPubkeyScript
+    {
+        public MockDeserializablePubScript(PubkeyScriptType pubT, int streamIndex, int bytesToRead, string errorToReturn = null)
+            : base(streamIndex, bytesToRead, errorToReturn)
+        {
+            typeToReturn = pubT;
+        }
+        public MockDeserializablePubScript(int streamIndex, int bytesToRead, string errorToReturn = null)
+            : this(PubkeyScriptType.Unknown, streamIndex, bytesToRead, errorToReturn)
+        {
+        }
+
+        private readonly PubkeyScriptType typeToReturn;
+
+        public PubkeyScriptType GetPublicScriptType() => typeToReturn;
     }
 }
