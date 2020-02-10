db033dd80599fe413530446b4f241c0e2d890c9c
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-02-10 10:00:09 +0330

    Change MainWindow to be used for testing Node and finding issues. This will be the entry point where initial Node's setup is taken from the user and Node is instantiated only once.

--- Src/Denovo/ViewModels/MainWindowViewModel.cs
+++ Src/Denovo/ViewModels/MainWindowViewModel.cs
@@ -3,27 +3,67 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
+using Autarkysoft.Bitcoin.P2PNetwork;
 using System;
+using System.Net;
 using System.Reflection;
 
 namespace Denovo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Message => "This application is still under construction.";
+        private readonly Node node = new Node();
+
+
+        private string _ip;
+        public string IpAddress
+        {
+            get => _ip;
+            set => SetField(ref _ip, value);
+        }
+
+        private string _res;
+        public string Result
+        {
+            get => _res;
+            set => SetField(ref _res, value);
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                Result = string.Empty;
+                if (IPAddress.TryParse(IpAddress, out IPAddress ip))
+                {
+                    node.StartConnect(new IPEndPoint(ip, 8333));
+                }
+                else
+                {
+                    Result = "Can't parse given IP address.";
+                }
+            }
+            catch (Exception ex)
+            {
+                Result = $"An exception of type {ex.GetType()} was thrown:{Environment.NewLine}{ex.Message}" +
+                    $"{Environment.NewLine}Stack trace:{Environment.NewLine}{ex.StackTrace}";
+            }
+
+        }
+
 
         public string Risk
         {
             get
             {
                 Version ver = Assembly.GetExecutingAssembly().GetName().Version;
+                string verInfo = ver.Major == 0 ?
+                    (ver.Minor == 0 ? "Version zero is incomplete [preview] release (high chance of having bugs)." :
+                    ver.Minor == 1 ? "First beta is a moderately stable version with little bugs but good chance of having more." :
+                    "Beta versions are moderately stable but have small chance of having unfound bugs.") :
+                    "Stable release";
 
-                return $"This is Denovo version {ver.ToString(4)}{Environment.NewLine}" +
-                    $"{((ver.Major == 0 && ver.Minor == 0) ? "That is version zero (incomplete preview release!)" : "")}" +
-                    $"{((ver.Major == 0 && ver.Minor == 1) ? "That is a beta release" : "")}" +
-                    $"{Environment.NewLine}" +
-                    $"If you use this application with real funds the risk of losing them is " +
-                    $"{(ver.Major == 0 && ver.Minor == 0 ? "100%" : ver.Minor < 5 ? "80%" : "50%")}";
+                return $"The current version is {ver.ToString(4)}{Environment.NewLine}{verInfo}";
             }
         }
     }
