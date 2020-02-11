c25fc3db7ef297dddb12785144e8f2d1f4934697
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-02-11 10:57:34 +0330

    Add TxPayload tests

--- Src/Autarkysoft.Bitcoin/P2PNetwork/Messages/MessagePayloads/TxPayload.cs
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/Messages/MessagePayloads/TxPayload.cs
@@ -4,13 +4,41 @@
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
 using Autarkysoft.Bitcoin.Blockchain.Transactions;
-using System;
 
 namespace Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads
 {
+    /// <summary>
+    /// A message payload containing a single transaction.
+    /// <para/> Sent: unsolicited, in response to <see cref="PayloadType.GetData"/>
+    /// </summary>
     public class TxPayload : PayloadBase
     {
-        public Transaction TxData { get; set; }
+        /// <summary>
+        /// Initializes an empty instance of <see cref="TxPayload"/>.
+        /// </summary>
+        public TxPayload()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TxPayload"/> using the given <see cref="ITransaction"/>.
+        /// </summary>
+        /// <param name="tx">Transaction to send</param>
+        public TxPayload(ITransaction tx)
+        {
+            Tx = tx;
+        }
+
+
+        private ITransaction _tx = new Transaction();
+        /// <summary>
+        /// The transaction to send
+        /// </summary>
+        public ITransaction Tx
+        {
+            get => _tx;
+            set => _tx = (value is null) ? new Transaction() : value;
+        }
 
 
         /// <inheritdoc/>
@@ -20,9 +48,8 @@ namespace Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads
         /// <inheritdoc/>
         public override void Serialize(FastStream stream)
         {
-            TxData.Serialize(stream);
+            Tx.Serialize(stream);
         }
-
 
         /// <inheritdoc/>
         public override bool TryDeserialize(FastStreamReader stream, out string error)
@@ -33,9 +60,7 @@ namespace Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads
                 return false;
             }
 
-            TxData = new Transaction();
-            return TxData.TryDeserialize(stream, out error);
+            return Tx.TryDeserialize(stream, out error);
         }
-
     }
 }
---
+++ Src/Tests/Bitcoin/P2PNetwork/Messages/MessagePayloads/TxPayloadTests.cs
@@ -0,0 +1,74 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin;
+using Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads;
+using System.Collections.Generic;
+using Tests.Bitcoin.Blockchain;
+using Xunit;
+
+namespace Tests.Bitcoin.P2PNetwork.Messages.MessagePayloads
+{
+    public class TxPayloadTests
+    {
+        [Fact]
+        public void SerializeTest()
+        {
+            TxPayload pl = new TxPayload()
+            {
+                Tx = new MockSerializableTx(new byte[] { 1, 2, 3 })
+            };
+
+            FastStream stream = new FastStream();
+            pl.Serialize(stream);
+            byte[] actual = pl.Serialize();
+            byte[] expected = new byte[] { 1, 2, 3 };
+
+            Assert.Equal(expected, stream.ToByteArray());
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TryDeserializeTest()
+        {
+            TxPayload pl = new TxPayload()
+            {
+                Tx = new MockDeserializableTx(0, 3)
+            };
+            FastStreamReader stream = new FastStreamReader(new byte[3]);
+            bool b = pl.TryDeserialize(stream, out string error);
+
+            Assert.True(b, error);
+            Assert.Null(error);
+            // Mock tx has its own tests.
+            Assert.Equal(PayloadType.Tx, pl.PayloadType);
+        }
+
+        public static IEnumerable<object[]> GetDeserFailCases()
+        {
+            yield return new object[] { null, null, "Stream can not be null." };
+            yield return new object[]
+            {
+                new FastStreamReader(new byte[1]),
+                new MockDeserializableTx(0, 1, "Foo"),
+                "Foo"
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetDeserFailCases))]
+        public void TryDeserialize_FailTest(FastStreamReader stream, MockDeserializableTx tx, string expErr)
+        {
+            TxPayload pl = new TxPayload()
+            {
+                Tx = tx
+            };
+
+            bool b = pl.TryDeserialize(stream, out string error);
+            Assert.False(b);
+            Assert.Equal(expErr, error);
+            // Mock tx has its own tests.
+        }
+    }
+}
