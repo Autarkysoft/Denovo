13ad5722a486414633d8d75e236873f20eda027b
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-02-16 10:15:16 +0330

    Add GetHeadersPayload tests

--- Src/Autarkysoft.Bitcoin/P2PNetwork/Messages/MessagePayloads/GetHeadersPayload.cs
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/Messages/MessagePayloads/GetHeadersPayload.cs
@@ -3,90 +3,34 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
-using System;
-
 namespace Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads
 {
-    // TODO: this is identical to GetHeadersPayload (except MaximumHashes)
-
-    public class GetHeadersPayload : PayloadBase
+    /// <summary>
+    /// A message payload containing the request for block headers.
+    /// <para/> Sent: unsolicited
+    /// </summary>
+    public class GetHeadersPayload : GetBlocksPayload
     {
-        private const int MaximumHashes = 2000;
-        public int Version { get; set; }
-        public byte[][] Hashes { get; set; }
-
-        private byte[] _stopHash;
-        public byte[] StopHash
+        /// <summary>
+        /// Initializes an empty instance of <see cref="GetHeadersPayload"/> used for deserialization.
+        /// </summary>
+        public GetHeadersPayload() : base()
         {
-            get => _stopHash;
-            set
-            {
-                if (value == null)
-                    throw new ArgumentNullException(nameof(StopHash), "Stop hash can not be null.");
-                if (value.Length != 32)
-                    throw new ArgumentOutOfRangeException(nameof(StopHash), "Stop hash must be 32 bytes.");
-
-                _stopHash = value;
-            }
         }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="GetHeadersPayload"/> using the given parameters.
+        /// </summary>
+        /// <inheritdoc/>
+        public GetHeadersPayload(int ver, byte[][] headerHashes, byte[] stopHash) : base(ver, headerHashes, stopHash)
+        {
+        }
+
+
+        /// <inheritdoc/>
+        protected override int MaximumHashes => 2000;
 
         /// <inheritdoc/>
         public override PayloadType PayloadType => PayloadType.GetHeaders;
-
-
-
-        /// <inheritdoc/>
-        public override void Serialize(FastStream stream)
-        {
-            CompactInt hashCount = new CompactInt(Hashes.Length);
-
-            stream.Write(Version);
-            hashCount.WriteToStream(stream);
-            foreach (var item in Hashes)
-            {
-                stream.Write(item);
-            }
-            stream.Write(StopHash);
-        }
-
-        /// <inheritdoc/>
-        public override bool TryDeserialize(FastStreamReader stream, out string error)
-        {
-            if (stream is null)
-            {
-                error = "Stream can not be null.";
-                return false;
-            }
-
-
-            if (!CompactInt.TryRead(stream, out CompactInt hashCount, out error))
-            {
-                return false;
-            }
-
-            if (hashCount > MaximumHashes)
-            {
-                error = $"GetHeaders message can not contain more than {MaximumHashes} header hashes.";
-                return false;
-            }
-
-            Hashes = new byte[(int)hashCount][];
-            for (int i = 0; i < (int)hashCount; i++)
-            {
-                if (!stream.TryReadByteArray(32, out Hashes[i]))
-                {
-                    return false;
-                }
-            }
-
-            if (!stream.TryReadByteArray(32, out _stopHash))
-            {
-                return false;
-            }
-
-            error = null;
-            return true;
-        }
-
     }
 }
---
+++ Src/Tests/Bitcoin/P2PNetwork/Messages/MessagePayloads/GetHeadersPayloadTests.cs
@@ -0,0 +1,114 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin;
+using Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.Bitcoin.P2PNetwork.Messages.MessagePayloads
+{
+    public class GetHeadersPayloadTests
+    {
+        [Fact]
+        public void Constructor_OutOfRangeExceptionTest()
+        {
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => new GetHeadersPayload(-1, new byte[1][], new byte[32]));
+            Assert.Contains("Version can not be negative.", ex.Message);
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => new GetHeadersPayload(1, new byte[2001][], new byte[32]));
+            Assert.Contains("Only a maximum of 2000 hashes are allowed.", ex.Message);
+
+            ex = Assert.Throws<ArgumentOutOfRangeException>(() => new GetHeadersPayload(1, new byte[1][], new byte[33]));
+            Assert.Contains("Stop hash length must be 32 bytes.", ex.Message);
+
+            ex = Assert.Throws<ArgumentNullException>(() => new GetHeadersPayload(1, null, new byte[32]));
+            Assert.Contains("Hash list can not be null.", ex.Message);
+
+            ex = Assert.Throws<ArgumentNullException>(() => new GetHeadersPayload(1, new byte[1][], null));
+            Assert.Contains("Stop hash can not be null.", ex.Message);
+        }
+
+        private const int Version = 70001;
+        private const string Header1 = "00000000000000001bd3146aa1555e10b23b63e6d484987237b575778a609fd3";
+        private const string Header2 = "00000000000000000aea3be27cda4b71011c2b60fb8a2e0a113708d403643e5c";
+        private const string PayloadHex = "71110100" + "02" + Header1 + Header2 +
+            "0000000000000000000000000000000000000000000000000000000000000000";
+
+        [Fact]
+        public void SerializeTest()
+        {
+            byte[] hd1 = Helper.HexToBytes(Header1);
+            byte[] hd2 = Helper.HexToBytes(Header2);
+            GetHeadersPayload pl = new GetHeadersPayload(Version, new byte[][] { hd1, hd2 }, new byte[32]);
+            FastStream stream = new FastStream(4 + 32 + 32 + 32);
+            pl.Serialize(stream);
+
+            byte[] actual = stream.ToByteArray();
+            byte[] expected = Helper.HexToBytes(PayloadHex);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TryDeserializeTest()
+        {
+            GetHeadersPayload pl = new GetHeadersPayload();
+            FastStreamReader stream = new FastStreamReader(Helper.HexToBytes(PayloadHex));
+            bool b = pl.TryDeserialize(stream, out string error);
+
+            byte[] hd1 = Helper.HexToBytes(Header1);
+            byte[] hd2 = Helper.HexToBytes(Header2);
+
+            Assert.True(b, error);
+            Assert.Null(error);
+            Assert.Equal(Version, pl.Version);
+            Assert.Equal(new byte[][] { hd1, hd2 }, pl.Hashes);
+            Assert.Equal(new byte[32], pl.StopHash);
+            Assert.Equal(PayloadType.GetHeaders, pl.PayloadType);
+        }
+
+        public static IEnumerable<object[]> GetDeserFailCases()
+        {
+            yield return new object[] { null, "Stream can not be null." };
+            yield return new object[] { new FastStreamReader(new byte[1]), Err.EndOfStream };
+            yield return new object[] { new FastStreamReader(new byte[4] { 255, 255, 255, 255 }), "Invalid version" };
+            yield return new object[]
+            {
+                new FastStreamReader(new byte[5] { 1, 0, 0, 0, 253 }),
+                "First byte 253 needs to be followed by at least 2 byte."
+            };
+            yield return new object[]
+            {
+                new FastStreamReader(new byte[7] { 1, 0, 0, 0, 0xfd, 0xd1, 0x07 }),
+                "Only 2000 hashes are accepted."
+            };
+            yield return new object[]
+            {
+                new FastStreamReader(new byte[5] { 1, 0, 0, 0, 1 }),
+                Err.EndOfStream
+            };
+            byte[] temp = new byte[38];
+            temp[0] = 1;
+            temp[4] = 1;
+            yield return new object[]
+            {
+                new FastStreamReader(temp),
+                Err.EndOfStream
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetDeserFailCases))]
+        public void TryDeserialize_FailTest(FastStreamReader stream, string expErr)
+        {
+            GetHeadersPayload pl = new GetHeadersPayload();
+
+            bool b = pl.TryDeserialize(stream, out string error);
+            Assert.False(b);
+            Assert.Equal(expErr, error);
+        }
+    }
+}
