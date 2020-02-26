662de0212012fc9476f7234ed139c9a4b91bf19b
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-02-26 08:50:06 +0330

    Add P2WPKH tests for transaction signing

--- Src/Tests/Bitcoin/Blockchain/Transactions/TransactionTests.cs
+++ Src/Tests/Bitcoin/Blockchain/Transactions/TransactionTests.cs
@@ -46,55 +46,57 @@ namespace Tests.Bitcoin.Blockchain.Transactions
         }
         public static IEnumerable<object[]> GetSignCases(TxCaseType mode)
         {
-            Transaction prevTx = new Transaction();
-            prevTx.TryDeserialize(new FastStreamReader(Helper.HexToBytes("0200000001443ae503e1a4082af188f50d946e22fededc37be8f327b4dce238852f58dec9c000000006a47304402203e1d50ffd7ee37405b562aa7e58c71503f6728ce888e43541d2c4f4edae6dc7d02205c0150d7cb1a6b9a15ed8cbfcbb0901cbd8356ba95b189a10cc78bb03c3a48f70121020ba6fef9cf323988bd76ce27d961adcf40a1ea152f26cc7d238d565cc9fef7a8ffffffff150e870100000000004341045cce0ce7f36105cb2e9779e37e06939327545f8cc64df4beb981162f6ed8961ba654949a16c977437fb22e54c5bfdd6473d54efef6735db0762038e30bde8071ac36870100000000002321035cce0ce7f36105cb2e9779e37e06939327545f8cc64df4beb981162f6ed8961bac04870100000000001976a914f814d73d5a89d2781048312718143e26b65008d988ac68870100000000001976a914c8f86d0b6ada16364003713d71facc3bbf7ba19588accc870100000000001976a914c8f86d0b6ada16364003713d71facc3bbf7ba19588ac30880100000000001976a91492c5966bdd571eca551036188a3853750d5a5c1b88ac3a880100000000001976a91492c5966bdd571eca551036188a3853750d5a5c1b88ac44880100000000001976a91492c5966bdd571eca551036188a3853750d5a5c1b88ac31880100000000001976a914dea359ebbf1d7e8617ac1e32cde802a0593abed588ac32880100000000001976a914dea359ebbf1d7e8617ac1e32cde802a0593abed588ac33880100000000001976a914dea359ebbf1d7e8617ac1e32cde802a0593abed588ac94880100000000001976a914d662fb8731e706b9adb7c11292223af0a56ac23288ac9e880100000000001976a914d662fb8731e706b9adb7c11292223af0a56ac23288aca8880100000000001976a914d662fb8731e706b9adb7c11292223af0a56ac23288acf8880100000000001976a9143f8a223cb05cae3c62c9b487b4de054503cbeff288ac02890100000000001976a9143f8a223cb05cae3c62c9b487b4de054503cbeff288ac0c890100000000001976a9143f8a223cb05cae3c62c9b487b4de054503cbeff288ac5c890100000000001976a914476abdf1bf9125fd4b49a420f66bf2f7c42d608888ac66890100000000001976a914476abdf1bf9125fd4b49a420f66bf2f7c42d608888ac70890100000000001976a914476abdf1bf9125fd4b49a420f66bf2f7c42d608888acef104002000000001976a914b50a33e43bec2c74112d1d6ea42a174f435aabd688ac00000000")), out string _);
-
             // Transactions are signed with bitcoin-core version 0.19.0.1 (bitcoin-0.19.0.1-x86_64-linux-gnu.tar)
             // All transactions are broadcast and are included in blocks on testnet.
             JArray jar = Helper.ReadResource<JArray>("SignedTxTestData");
-            foreach (var item in jar)
+            foreach (var Case in jar)
             {
-                if (mode == TxCaseType.BytesToSign || mode == TxCaseType.FinalSignedTx)
+                Transaction prevTx = new Transaction();
+                prevTx.TryDeserialize(new FastStreamReader(Helper.HexToBytes(Case["TxToSpend"].ToString())), out string _);
+                foreach (var item in Case["Cases"])
                 {
-                    Transaction tx = new Transaction();
-                    FastStreamReader stream = new FastStreamReader(Helper.HexToBytes(item["TxToSign"].ToString()));
-                    if (!tx.TryDeserialize(stream, out string err))
+                    if (mode == TxCaseType.BytesToSign || mode == TxCaseType.FinalSignedTx)
                     {
-                        throw new ArgumentException($"Could not deseralize the given tx case: " +
-                            $"{item["TestName"]}. Error: {err}");
-                    }
-                    SigHashType sht = (SigHashType)(uint)item["SigHashType"];
-                    int[] indexes = item["Indexes"].ToObject<int[]>();
+                        Transaction tx = new Transaction();
+                        FastStreamReader stream = new FastStreamReader(Helper.HexToBytes(item["TxToSign"].ToString()));
+                        if (!tx.TryDeserialize(stream, out string err))
+                        {
+                            throw new ArgumentException($"Could not deseralize the given tx case: " +
+                                $"{item["TestName"]}. Error: {err}");
+                        }
+                        SigHashType sht = (SigHashType)(uint)item["SigHashType"];
+                        int[] indexes = item["Indexes"].ToObject<int[]>();
 
-                    if (mode == TxCaseType.BytesToSign)
+                        if (mode == TxCaseType.BytesToSign)
+                        {
+                            string[] hashes = item["BytesToSign"].ToObject<string[]>();
+                            yield return new object[] { tx, prevTx, sht, indexes, hashes };
+                        }
+                        else
+                        {
+                            string key = item["PrivateKey"].ToString();
+                            byte[] signedTx = Helper.HexToBytes(item["SignedTx"].ToString());
+                            yield return new object[] { key, tx, prevTx, sht, indexes, signedTx };
+                        }
+                    }
+                    else if (mode == TxCaseType.TxId)
                     {
-                        string[] hashes = item["BytesToSign"].ToObject<string[]>();
-                        yield return new object[] { tx, prevTx, sht, indexes, hashes };
+                        Transaction tx = new Transaction();
+                        FastStreamReader stream = new FastStreamReader(Helper.HexToBytes(item["SignedTx"].ToString()));
+                        if (!tx.TryDeserialize(stream, out string err))
+                        {
+                            throw new ArgumentException($"Could not deseralize the given tx case: " +
+                                $"{item["TestName"]}. Error: {err}");
+                        }
+                        string txId = item["TxId"].ToString();
+                        string wtxId = item["WTxId"].ToString();
+
+                        yield return new object[] { tx, txId, wtxId };
                     }
                     else
                     {
-                        string key = item["PrivateKey"].ToString();
-                        byte[] signedTx = Helper.HexToBytes(item["SignedTx"].ToString());
-                        yield return new object[] { key, tx, prevTx, sht, indexes, signedTx };
+                        throw new ArgumentException("Undefined mode");
                     }
-                }
-                else if (mode == TxCaseType.TxId)
-                {
-                    Transaction tx = new Transaction();
-                    FastStreamReader stream = new FastStreamReader(Helper.HexToBytes(item["SignedTx"].ToString()));
-                    if (!tx.TryDeserialize(stream, out string err))
-                    {
-                        throw new ArgumentException($"Could not deseralize the given tx case: " +
-                            $"{item["TestName"]}. Error: {err}");
-                    }
-                    string txId = item["TxId"].ToString();
-                    string wtxId = item["WTxId"].ToString();
-
-                    yield return new object[] { tx, txId, wtxId };
-                }
-                else
-                {
-                    throw new ArgumentException("Undefined mode");
                 }
             }
         }
