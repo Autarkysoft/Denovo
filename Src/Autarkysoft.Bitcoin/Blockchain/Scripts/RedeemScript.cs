0b1d34fc280ecf8fb1e4d834595b6891b8365126
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-02-27 18:10:11 +0330

    Add RedeemScript tests + some fixes

--- Src/Autarkysoft.Bitcoin/Blockchain/Scripts/RedeemScript.cs
+++ Src/Autarkysoft.Bitcoin/Blockchain/Scripts/RedeemScript.cs
@@ -19,7 +19,7 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
         /// <summary>
         /// Initializes a new instance of <see cref="RedeemScript"/>.
         /// </summary>
-        public RedeemScript() : base(10000)
+        public RedeemScript() : base(520)
         {
             ScriptType = ScriptType.ScriptRedeem;
             witHashFunc = new Sha256(false);
@@ -27,18 +27,12 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
 
 
 
-        private const int MinMultiPubCount = 0;
-        private const int MaxMultiPubCount = 20;
-
         private readonly Ripemd160Sha256 hashFunc = new Ripemd160Sha256();
         private readonly Sha256 witHashFunc = new Sha256(false);
 
 
 
-        /// <summary>
-        /// Returns <see cref="RedeemScriptType"/> type of this instance if it was defined.
-        /// </summary>
-        /// <returns>The <see cref="RedeemScriptType"/></returns>
+        /// <inheritdoc/>
         public RedeemScriptType GetRedeemScriptType()
         {
             if (OperationList == null || OperationList.Length == 0)
@@ -46,8 +40,8 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
                 return RedeemScriptType.Empty;
             }
             else if (OperationList.Length == 2 &&
-                OperationList[0] is PushDataOp && OperationList[0].OpValue == OP._0 &&
-                OperationList[1] is PushDataOp push1)
+                     OperationList[0] is PushDataOp op0 && op0.OpValue == OP._0 &&
+                     OperationList[1] is PushDataOp push1)
             {
                 if (push1.data?.Length == hashFunc.HashByteSize)
                 {
@@ -59,14 +53,20 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
                 }
             }
             else if (OperationList.Length == 5 &&
-                    OperationList[0] is PushDataOp push2 && push2.data?.Length > 1 && push2.data.Length < 5 &&
-                    OperationList[1] is CheckLocktimeVerifyOp &&
-                    OperationList[2] is DROPOp &&
-                    OperationList[3] is PushDataOp push3 &&
-                    (push3.data?.Length == CompPubKeyLength || push3.data?.Length == UncompPubKeyLength) &&
-                    OperationList[4] is CheckSigOp)
+                     OperationList[0] is PushDataOp push2 && push2.TryGetNumber(out _, out _, true, 5) &&
+                     OperationList[1] is CheckLocktimeVerifyOp &&
+                     OperationList[2] is DROPOp &&
+                     OperationList[3] is PushDataOp push3 &&
+                     (push3.data?.Length == CompPubKeyLength || push3.data?.Length == UncompPubKeyLength) &&
+                     OperationList[4] is CheckSigOp)
             {
                 return RedeemScriptType.CheckLocktimeVerify;
+            }
+            else if (OperationList[^1] is CheckMultiSigOp)
+            {
+                // Multi-sig redeem scripts can be as simple as a single CheckMultiSigOp OP (pointless script that anyone can spend)
+                // to the usual multi-sig scripts with (OP_m <n*pubkeys> OP_n OP_CheckMultiSig) script up to 15 pubkeys
+                return RedeemScriptType.MultiSig;
             }
 
             return RedeemScriptType.Unknown;
@@ -74,45 +74,60 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
 
 
         /// <summary>
-        /// 
+        /// Sets this script to a "m of n multi-signature" script using the given <see cref="PublicKey"/>s.
         /// </summary>
-        /// <param name="m"></param>
-        /// <param name="n"></param>
-        /// <param name="pubKeyList"></param>
-        public void SetToMultiSig(int m, int n, Tuple<PublicKey, bool>[] pubKeyList)
+        /// <remarks>
+        /// Since normally a m-of-n redeem script is created from all compressed or all uncompressed public keys
+        /// this method has one boolean determining that. If a mixture is needed the <see cref="IScript.OperationList"/>
+        /// should be set manually.
+        /// Additionally m and n must be at least 1 (although 0 is valid) to prevent insecure redeem scripts (anyone can spend).
+        /// </remarks>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <param name="m">
+        /// The minimum number of required signatures (must be smaller than <paramref name="pubKeys"/>.Length and be at least 1).
+        /// </param>
+        /// <param name="pubKeys">
+        /// A list of public keys to use (must contain at least 1 public key up to 15 compressed or 7 uncompressed keys)
+        /// </param>
+        /// <param name="useCompressed">
+        /// [Default value = true] If true compressed public keys are used, uncompressed otherwise
+        /// </param>
+        public void SetToMultiSig(int m, PublicKey[] pubKeys, bool useCompressed = true)
         {
-            // TODO: read
-            // https://github.com/bitcoin/bitcoin/blob/871d3ae45b642c903ed9c54235c09d62f6cf9a16/src/script/script.h#L22-L35
-            // https://bitcoin.stackexchange.com/questions/23893/what-are-the-limits-of-m-and-n-in-m-of-n-multisig-addresses
-
-            if (m < MinMultiPubCount || m > MaxMultiPubCount || m > n)
+            if (pubKeys == null || pubKeys.Length == 0)
+                throw new ArgumentNullException(nameof(pubKeys), "Pubkey list can not be null or empty.");
+            // Maximum allowed length of a redeem script is 520 bytes. That is:
+            // Compressed 3 + 15*(1+33) = 513 or uncompressed 3 + 7*(1+65) = 465
+            if (m < 1 || (useCompressed && m > 15) || (!useCompressed && m > 7) || m > pubKeys.Length)
             {
                 throw new ArgumentOutOfRangeException(nameof(m),
-                    $"M must be between {MinMultiPubCount} and {MaxMultiPubCount} and smaller than N.");
+                    "M must be between 1 and (7 uncomprssed or 15 compressed) public keys and smaller than N.");
             }
-            if (n < MinMultiPubCount || n > MaxMultiPubCount)
-                throw new ArgumentOutOfRangeException(nameof(n), $"N must be between {MinMultiPubCount} and {MaxMultiPubCount}.");
-            if (pubKeyList == null || pubKeyList.Length == 0)
-                throw new ArgumentNullException(nameof(pubKeyList), "Pubkey list can not be null or empty.");
-            if (pubKeyList.Length != n)
-                throw new ArgumentOutOfRangeException(nameof(pubKeyList), $"Pubkey list must contain N (={n}) items.");
+            if ((useCompressed && pubKeys.Length > 15) || (!useCompressed && pubKeys.Length > 7))
+            {
+                throw new ArgumentOutOfRangeException(nameof(pubKeys),
+                    "Pubkey list must contain at least 1 and at most 15 compressed or 7 uncompressed keys.");
+            }
 
             // OP_m | pub1 | pub2 | ... | pub(n) | OP_n | OP_CheckMultiSig
-            OperationList = new IOperation[n + 3];
+            OperationList = new IOperation[pubKeys.Length + 3];
             OperationList[0] = new PushDataOp(m);
-            OperationList[n + 1] = new PushDataOp(n);
-            OperationList[n + 2] = new CheckMultiSigOp();
+            OperationList[^2] = new PushDataOp(pubKeys.Length);
+            OperationList[^1] = new CheckMultiSigOp();
             int i = 1;
-            foreach (var item in pubKeyList)
+            foreach (var item in pubKeys)
             {
-                OperationList[i++] = new PushDataOp(item.Item1.ToByteArray(item.Item2));
+                OperationList[i++] = new PushDataOp(item.ToByteArray(useCompressed));
             }
         }
 
+
         /// <summary>
-        /// 
+        /// Sets this script to a P2SH-P2WPKH redeem script using the given parameters.
         /// </summary>
-        /// <param name="pubKey"></param>
+        /// <exception cref="ArgumentNullException"/>
+        /// <param name="pubKey">Public key to use</param>
         /// <param name="useCompressed">
         /// [Default value = true]
         /// Indicates whether to use compressed or uncompressed public key in the redeem script.
@@ -120,6 +135,9 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
         /// </param>
         public void SetToP2SH_P2WPKH(PublicKey pubKey, bool useCompressed = true)
         {
+            if (pubKey is null)
+                throw new ArgumentNullException(nameof(pubKey), "Public key can not be null.");
+
             byte[] hash = hashFunc.ComputeHash(pubKey.ToByteArray(useCompressed));
             OperationList = new IOperation[]
             {
@@ -128,8 +146,17 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
             };
         }
 
+
+        /// <summary>
+        /// Sets this script to a P2SH-P2WSH redeem script using the given <see cref="IScript"/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <param name="witnessScript">Script to use</param>
         public void SetToP2SH_P2WSH(IScript witnessScript)
         {
+            if (witnessScript is null)
+                throw new ArgumentNullException(nameof(witnessScript), "Witness script can not be null.");
+
             byte[] hash = witHashFunc.ComputeHash(witnessScript.ToByteArray());
             OperationList = new IOperation[]
             {
@@ -139,7 +166,7 @@ namespace Autarkysoft.Bitcoin.Blockchain.Scripts
         }
 
 
-
+        // TODO: is this used anywhere?
         public PubkeyScript ConvertP2WPKH_to_P2PKH()
         {
             if (GetRedeemScriptType() != RedeemScriptType.P2SH_P2WPKH)
---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/RedeemScriptTests.cs
@@ -0,0 +1,344 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+using Autarkysoft.Bitcoin.Cryptography.Asymmetric.KeyPairs;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Scripts
+{
+    public class RedeemScriptTests
+    {
+        // 2 randomly generated public keys:
+        private static readonly byte[] pubBaC1 = Helper.HexToBytes("02a17d82262d4ab8d9499d664c637c075a3ddc1bf2bb0b392188ba9e1043514ffd");
+        private static readonly byte[] pubBaUC1 = Helper.HexToBytes("04a17d82262d4ab8d9499d664c637c075a3ddc1bf2bb0b392188ba9e1043514ffd2ec1051619dec03da6be55608dff5a2a800907e8358b3b76ea86f90f22cd2fc6");
+        private static readonly byte[] pubBaC1_hash = Helper.HexToBytes("03814c6125f6ac2ebfc42d74339af43dc7530313");
+        private static readonly byte[] pubBaUC1_hash = Helper.HexToBytes("7f8b56fd6eeb910db9c0bca69aebada4d3e16d6f");
+
+        private static readonly byte[] pubBaC2 = Helper.HexToBytes("02a63ea6d772bc7127a67be7fc4310164737430de14003c1d7c3f8d5a190f3dfd0");
+        private static readonly byte[] pubBaUC2 = Helper.HexToBytes("04a63ea6d772bc7127a67be7fc4310164737430de14003c1d7c3f8d5a190f3dfd033fbf623236b512c709008c11307453980a926fd361968c7e039375d9662f228");
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            RedeemScript scr = new RedeemScript();
+
+            Assert.Empty(scr.OperationList);
+            Assert.False(scr.IsWitness);
+            Assert.Equal(ScriptType.ScriptRedeem, scr.ScriptType);
+        }
+
+        public static IEnumerable<object[]> GetScrTypeCases()
+        {
+            yield return new object[] { new RedeemScript(), RedeemScriptType.Empty };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new DUPOp() } },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(new byte[20]) } },
+                RedeemScriptType.P2SH_P2WPKH
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._1), new PushDataOp(new byte[20]) } },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(new byte[21]) } },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(OP._1) } },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[] { new PushDataOp(new byte[20]), new PushDataOp(new byte[20]) }
+                },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(new byte[32]) } },
+                RedeemScriptType.P2SH_P2WSH
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._1), new PushDataOp(new byte[32]) } },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript() { OperationList = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(new byte[33]) } },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[]
+                    {
+                        new PushDataOp(123),
+                        new CheckLocktimeVerifyOp(),
+                        new DROPOp(),
+                        new PushDataOp(new byte[33]),
+                        new CheckSigOp()
+                    }
+                },
+                RedeemScriptType.CheckLocktimeVerify
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[]
+                    {
+                        new PushDataOp(123),
+                        new CheckLocktimeVerifyOp(),
+                        new DROPOp(),
+                        new PushDataOp(new byte[65]),
+                        new CheckSigOp()
+                    }
+                },
+                RedeemScriptType.CheckLocktimeVerify
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[]
+                    {
+                        new PushDataOp(new byte[6]),
+                        new CheckLocktimeVerifyOp(),
+                        new DROPOp(),
+                        new PushDataOp(new byte[33]),
+                        new CheckSigOp()
+                    }
+                },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[]
+                    {
+                        new PushDataOp(123),
+                        new CheckLocktimeVerifyOp(),
+                        new DROPOp(),
+                        new PushDataOp(new byte[34]),
+                        new CheckSigOp()
+                    }
+                },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[]
+                    {
+                        new PushDataOp(123),
+                        new CheckLocktimeVerifyOp(),
+                        new DROPOp(),
+                        new PushDataOp(OP._1),
+                        new CheckSigOp()
+                    }
+                },
+                RedeemScriptType.Unknown
+            };
+            yield return new object[]
+            {
+                new RedeemScript()
+                {
+                    OperationList = new IOperation[]
+                    {
+                        new CheckMultiSigOp()
+                    }
+                },
+                RedeemScriptType.MultiSig
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetScrTypeCases))]
+        public void GetRedeemScriptTypeTest(IRedeemScript scr, RedeemScriptType expected)
+        {
+            RedeemScriptType actual = scr.GetRedeemScriptType();
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetSetToMultiSigCases()
+        {
+            PublicKey.TryRead(pubBaUC1, out PublicKey pub1);
+            PublicKey.TryRead(pubBaUC2, out PublicKey pub2);
+
+            yield return new object[]
+            {
+                1, new PublicKey[] { pub1 }, true,
+                new IOperation[] { new PushDataOp(1), new PushDataOp(pubBaC1), new PushDataOp(1), new CheckMultiSigOp() }
+            };
+            yield return new object[]
+            {
+                1, new PublicKey[] { pub1 }, false,
+                new IOperation[] { new PushDataOp(1), new PushDataOp(pubBaUC1), new PushDataOp(1), new CheckMultiSigOp() }
+            };
+            yield return new object[]
+            {
+                1, new PublicKey[] { pub1, pub2 }, true,
+                new IOperation[]
+                {
+                    new PushDataOp(1), new PushDataOp(pubBaC1), new PushDataOp(pubBaC2), new PushDataOp(2), new CheckMultiSigOp()
+                }
+            };
+            yield return new object[]
+            {
+                1, new PublicKey[] { pub1, pub2 }, false,
+                new IOperation[]
+                {
+                    new PushDataOp(1), new PushDataOp(pubBaUC1), new PushDataOp(pubBaUC2), new PushDataOp(2), new CheckMultiSigOp()
+                }
+            };
+            yield return new object[]
+            {
+                2, new PublicKey[] { pub1, pub2 }, false,
+                new IOperation[]
+                {
+                    new PushDataOp(2), new PushDataOp(pubBaUC1), new PushDataOp(pubBaUC2), new PushDataOp(2), new CheckMultiSigOp()
+                }
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetSetToMultiSigCases))]
+        public void SetToMultiSigTest(int m, PublicKey[] pubs, bool comp, IOperation[] expected)
+        {
+            RedeemScript scr = new RedeemScript();
+            scr.SetToMultiSig(m, pubs, comp);
+
+            Assert.Equal(expected, scr.OperationList);
+        }
+
+        public static IEnumerable<object[]> GetSetToMultiSigFailCases()
+        {
+            yield return new object[]
+            {
+                -1, new PublicKey[2], true,
+                "M must be between 1 and (7 uncomprssed or 15 compressed) public keys and smaller than N."
+            };
+            yield return new object[]
+            {
+                0, new PublicKey[2], true,
+                "M must be between 1 and (7 uncomprssed or 15 compressed) public keys and smaller than N."
+            };
+            yield return new object[]
+            {
+                3, new PublicKey[2], true,
+                "M must be between 1 and (7 uncomprssed or 15 compressed) public keys and smaller than N."
+            };
+            yield return new object[]
+            {
+                16, new PublicKey[16], true,
+                "M must be between 1 and (7 uncomprssed or 15 compressed) public keys and smaller than N."
+            };
+            yield return new object[]
+            {
+                8, new PublicKey[8], false,
+                "M must be between 1 and (7 uncomprssed or 15 compressed) public keys and smaller than N."
+            };
+            yield return new object[]
+            {
+                1, new PublicKey[16], true,
+                "Pubkey list must contain at least 1 and at most 15 compressed or 7 uncompressed keys."
+            };
+            yield return new object[]
+            {
+                1, new PublicKey[8], false,
+                "Pubkey list must contain at least 1 and at most 15 compressed or 7 uncompressed keys."
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetSetToMultiSigFailCases))]
+        public void SetToMultiSig_OutOfRangeExceptionTest(int m, PublicKey[] pubs, bool comp, string expError)
+        {
+            RedeemScript scr = new RedeemScript();
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => scr.SetToMultiSig(m, pubs, comp));
+            Assert.Contains(expError, ex.Message);
+        }
+
+        [Fact]
+        public void SetToMultiSig_NullExceptionTest()
+        {
+            RedeemScript scr = new RedeemScript();
+            Assert.Throws<ArgumentNullException>(() => scr.SetToMultiSig(1, null, true));
+            Assert.Throws<ArgumentNullException>(() => scr.SetToMultiSig(1, new PublicKey[0], true));
+        }
+
+
+        [Fact]
+        public void SetToP2SH_P2WPKH_CompTest()
+        {
+            RedeemScript scr = new RedeemScript();
+            PublicKey.TryRead(pubBaUC1, out PublicKey pub);
+            scr.SetToP2SH_P2WPKH(pub, true);
+
+            IOperation[] expected = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(pubBaC1_hash) };
+
+            Assert.Equal(expected, scr.OperationList);
+        }
+
+        [Fact]
+        public void SetToP2SH_P2WPKH_UnCompTest()
+        {
+            RedeemScript scr = new RedeemScript();
+            PublicKey.TryRead(pubBaUC1, out PublicKey pub);
+            // This is non-standard
+            scr.SetToP2SH_P2WPKH(pub, false);
+
+            IOperation[] expected = new IOperation[] { new PushDataOp(OP._0), new PushDataOp(pubBaUC1_hash) };
+
+            Assert.Equal(expected, scr.OperationList);
+        }
+
+        [Fact]
+        public void SetToP2SH_P2WPKH_ExceptionTest()
+        {
+            RedeemScript scr = new RedeemScript();
+            Assert.Throws<ArgumentNullException>(() => scr.SetToP2SH_P2WPKH(null, false));
+        }
+
+
+        [Fact]
+        public void SetToP2SH_P2WSHTest()
+        {
+            RedeemScript scr = new RedeemScript();
+            MockSerializableScript mockScr = new MockSerializableScript(new byte[] { 1, 2, 3 });
+            scr.SetToP2SH_P2WSH(mockScr);
+
+            IOperation[] expected = new IOperation[]
+            {
+                new PushDataOp(OP._0),
+                new PushDataOp(Helper.HexToBytes("039058c6f2c0cb492c533b0a4d14ef77cc0f78abccced5287d84a1a2011cfb81"))
+            };
+
+            Assert.Equal(expected, scr.OperationList);
+        }
+
+        [Fact]
+        public void SetToP2SH_P2WSH_ExceptionTest()
+        {
+            RedeemScript scr = new RedeemScript();
+            Assert.Throws<ArgumentNullException>(() => scr.SetToP2SH_P2WSH(null));
+        }
+    }
+}
