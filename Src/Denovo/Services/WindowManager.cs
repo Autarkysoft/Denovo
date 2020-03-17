3dd3d0143312d244c401c00e8670123df4c84a36
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-03-17 10:07:47 +0330

    Add a window manager

---
+++ Src/Denovo/Services/WindowManager.cs
@@ -0,0 +1,39 @@
+﻿// Denovo
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Denovo.ViewModels;
+
+namespace Denovo.Services
+{
+    public interface IWindowManager
+    {
+        void ShowDialog(VmWithSizeBase vm);
+    }
+
+
+    public class WindowManager : IWindowManager
+    {
+        public void ShowDialog(VmWithSizeBase vm)
+        {
+            Window win = new Window()
+            {
+                Content = vm,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                CanResize = false,
+                ShowInTaskbar = false,
+                Width = vm.Width,
+                Height = vm.Height,
+                Title = vm.GetType().Name.Replace("ViewModel", ""),
+            };
+
+            var lf = (IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime;
+
+            win.ShowDialog(lf.MainWindow);
+        }
+    }
+}
---
+++ Src/Denovo/ViewModels/VmWithSizeBase.cs
@@ -0,0 +1,35 @@
+﻿// Denovo
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+namespace Denovo.ViewModels
+{
+    /// <summary>
+    /// Base (abstract) class for view models that have to be shown in a new window 
+    /// and need to set the window's height and width.
+    /// </summary>
+    public abstract class VmWithSizeBase : ViewModelBase
+    {
+        public VmWithSizeBase(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+
+        private double _height;
+        public double Height
+        {
+            get => _height;
+            set => SetField(ref _height, value);
+        }
+
+        private double _width;
+        public double Width
+        {
+            get => _width;
+            set => SetField(ref _width, value);
+        }
+    }
+}
