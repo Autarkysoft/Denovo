ff139dc94dee6687bd662990cc0574a658bf25a9
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-03-27 16:33:32 +0430

    Add MockOp for tests

---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/MockOp.cs
@@ -0,0 +1,38 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+
+namespace Tests.Bitcoin.Blockchain.Scripts.Operations
+{
+    internal class MockOp : IOperation
+    {
+        public MockOp(bool runSuccessful, string errorIfFail, OP opValue = (OP)255)
+        {
+            succeed = runSuccessful;
+            errMsg = errorIfFail;
+            OpValue = opValue;
+        }
+
+        private readonly string errMsg;
+        private readonly bool succeed;
+        public OP OpValue { get; }
+
+        public bool Run(IOpData opData, out string error)
+        {
+            if (succeed)
+            {
+                error = null;
+                return true;
+            }
+            else
+            {
+                error = errMsg;
+                return false;
+            }
+        }
+    }
+}
