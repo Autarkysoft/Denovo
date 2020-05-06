a2a0d9a86639d615292d8ea72a00b88550d35d78
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-05-06 10:54:37 +0430

    Add BIP-38 + tests

---
+++ Src/Autarkysoft.Bitcoin/ImprovementProposals/BIP0038.cs
@@ -0,0 +1,276 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Cryptography.Asymmetric.KeyPairs;
+using Autarkysoft.Bitcoin.Cryptography.Hashing;
+using Autarkysoft.Bitcoin.Cryptography.KeyDerivationFunctions;
+using Autarkysoft.Bitcoin.Encoders;
+using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Autarkysoft.Bitcoin.ImprovementProposals
+{
+    /// <summary>
+    /// Passphrase-protected private key. Implements <see cref="IDisposable"/>.
+    /// <para/>https://github.com/bitcoin/bips/blob/master/bip-0038.mediawiki
+    /// </summary>
+    public class BIP0038 : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="BIP0038"/>.
+        /// </summary>
+        public BIP0038()
+        {
+            b58enc = new Base58();
+            addressMaker = new Address();
+            scrypt = new Scrypt(16384, 8, 8);
+            hash = new Sha256(true);
+            aes = new AesManaged
+            {
+                KeySize = 256, // AES-256
+                Mode = CipherMode.ECB, // This mode encrypts each block individually
+                IV = new byte[16], // No initialization vector is used
+                Padding = PaddingMode.None // No padding
+            };
+        }
+
+        // TODO: add code for EC multiply types
+
+
+        private const int EncodedLength = 39;
+        private readonly byte[] prefix = { 0x01, 0x42 };
+        private readonly byte[] prefix_ECMultiplied = { 0x01, 0x43 };
+        private readonly Base58 b58enc;
+        private readonly Address addressMaker;
+        private Scrypt scrypt;
+        private Aes aes;
+        private IHashFunction hash;
+
+
+
+        /// <summary>
+        /// Decrypts the given base-58 encoded encrypted key using the given password string and first 4 bytes of hash of
+        /// (un)compressed P2PKH address as <see cref="Scrypt"/> salt.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="FormatException"/>
+        /// <exception cref="ObjectDisposedException"/>
+        /// <param name="encrypted">Base-58 encrypted key (it will be normalized using Unicode Normalization Form C (NFC))</param>
+        /// <param name="password">Password to use</param>
+        /// <param name="isCompressed">Indicates whether to use compressed or uncompressed public key to build P2PKH address</param>
+        /// <returns>The private key</returns>
+        public PrivateKey Decrypt(string encrypted, string password, out bool isCompressed)
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(nameof(BIP0038), "Instance was disposed.");
+            if (string.IsNullOrWhiteSpace(encrypted))
+                throw new ArgumentNullException(nameof(encrypted), "Encrypted string can not be null.");
+            // Note that empty password string (ie: "") should not be rejected for decryption
+            if (password == null)
+                throw new ArgumentNullException(nameof(password), "Password can not be null.");
+
+            byte[] pass = Encoding.UTF8.GetBytes(password.Normalize(NormalizationForm.FormC));
+            return Decrypt(encrypted, pass, out isCompressed);
+        }
+
+        /// <summary>
+        /// Decrypts the given base-58 encoded encrypted key using the given password bytes and first 4 bytes of hash of
+        /// (un)compressed P2PKH address as <see cref="Scrypt"/> salt.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="FormatException"/>
+        /// <exception cref="ObjectDisposedException"/>
+        /// <param name="encrypted">Base-58 encrypted key (it will be normalized using Unicode Normalization Form C (NFC))</param>
+        /// <param name="password">Password to use</param>
+        /// <param name="isCompressed">Indicates whether to use compressed or uncompressed public key to build P2PKH address</param>
+        /// <returns>The private key</returns>
+        public PrivateKey Decrypt(string encrypted, byte[] password, out bool isCompressed)
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(nameof(BIP0038), "Instance was disposed.");
+            if (string.IsNullOrWhiteSpace(encrypted))
+                throw new ArgumentNullException(nameof(encrypted), "Invalid (null) encrypted key.");
+            if (password == null)
+                throw new ArgumentNullException(nameof(password), "Password can not be null.");
+
+
+            byte[] encryptedBytes = b58enc.DecodeWithCheckSum(encrypted);
+            if (encryptedBytes.Length != EncodedLength)
+            {
+                throw new FormatException("Invalid encrypted bytes length.");
+            }
+
+            if (!((Span<byte>)encryptedBytes).Slice(0, 2).SequenceEqual(prefix))
+            {
+                throw new FormatException("Invalid prefix.");
+            }
+
+            isCompressed = IsCompressed(encryptedBytes[2]);
+
+            Span<byte> salt = ((Span<byte>)encryptedBytes).Slice(3, 4);
+
+            byte[] dk = scrypt.GetBytes(password, salt.ToArray(), 64);
+            byte[] decryptedResult = new byte[32];
+
+            aes.Key = dk.SubArray(32, 32); // AES key is derivedhalf2
+            using ICryptoTransform decryptor = aes.CreateDecryptor();
+            decryptor.TransformBlock(encryptedBytes, 7, 16, decryptedResult, 0);
+            decryptor.TransformBlock(encryptedBytes, 23, 16, decryptedResult, 16);
+
+            // XOR method will only work on first item's length (32 byte here) so it doesn't matter of dk.Legth is 64
+            PrivateKey result = new PrivateKey(XOR(decryptedResult, dk));
+
+            string address = addressMaker.GetP2pkh(result.ToPublicKey(), isCompressed, NetworkType.MainNet);
+            Span<byte> computedHash = hash.ComputeHash(Encoding.ASCII.GetBytes(address)).SubArray(0, 4);
+            if (!computedHash.SequenceEqual(salt))
+            {
+                throw new FormatException("Wrong password (derived address hash is not the same).");
+            }
+
+            return result;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsCompressed(byte b) => (b & 0b0010_0000) != 0;
+
+
+        /// <summary>
+        /// Encrypts the given private key using the password string and first 4 bytes of hash of (un)compressed P2PKH address
+        /// as <see cref="Scrypt"/> salt.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ObjectDisposedException"/>
+        /// <param name="key">Private key to encrypt</param>
+        /// <param name="password">Password to use (it will be normalized using Unicode Normalization Form C (NFC))</param>
+        /// <param name="isCompressed">Indicates whether to use compressed or uncompressed public key to build P2PKH address</param>
+        /// <returns>Base-58 encoded encrypted result with a checksum</returns>
+        public string Encrypt(PrivateKey key, string password, bool isCompressed)
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(nameof(BIP0038), "Instance was disposed.");
+            if (key is null)
+                throw new ArgumentNullException(nameof(key), "Private key can not be null.");
+            // Note that empty password string (ie: "") is rejected for encryption
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password), "Password can not be null or empty.");
+
+            return Encrypt(key, Encoding.UTF8.GetBytes(password.Normalize(NormalizationForm.FormC)), isCompressed);
+        }
+
+        /// <summary>
+        /// Encrypts the given private key using the password bytes and first 4 bytes of hash of (un)compressed P2PKH address
+        /// as <see cref="Scrypt"/> salt.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ObjectDisposedException"/>
+        /// <param name="key">Private key to encrypt</param>
+        /// <param name="password">Password to use</param>
+        /// <param name="isCompressed">Indicates whether to use compressed or uncompressed public key to build P2PKH address</param>
+        /// <returns>Base-58 encoded encrypted result with a checksum</returns>
+        public string Encrypt(PrivateKey key, byte[] password, bool isCompressed)
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(nameof(BIP0038), "Instance was disposed.");
+            if (key is null)
+                throw new ArgumentNullException(nameof(key), "Private key can not be null.");
+            // Note that empty password is rejected for encryption
+            if (password == null || password.Length == 0)
+                throw new ArgumentNullException(nameof(password), "Password can not be null or empty.");
+
+            string address = addressMaker.GetP2pkh(key.ToPublicKey(), isCompressed, NetworkType.MainNet);
+            byte[] salt = hash.ComputeHash(Encoding.ASCII.GetBytes(address)).SubArray(0, 4);
+
+            byte[] dk = scrypt.GetBytes(password, salt, 64);
+            aes.Key = dk.SubArray(32, 32); // AES key is derivedhalf2
+            byte[] encryptedResult = new byte[32];
+            // XOR method will only work on first item's length (32 byte here) so it doesn't matter of dk.Legth is 64
+            byte[] block = XOR(key.ToBytes(), dk);
+
+            using ICryptoTransform encryptor = aes.CreateEncryptor();
+            encryptor.TransformBlock(block, 0, 16, encryptedResult, 0);
+            encryptor.TransformBlock(block, 16, 16, encryptedResult, 16);
+
+            FastStream stream = new FastStream(EncodedLength);
+            stream.Write(prefix);
+            stream.Write(GetFlagByte(isCompressed, false));
+            stream.Write(salt);
+            stream.Write(encryptedResult);
+
+            return b58enc.EncodeWithCheckSum(stream.ToByteArray());
+        }
+
+        private byte[] XOR(byte[] first, byte[] second)
+        {
+            byte[] result = new byte[first.Length];
+            for (int i = 0; i < first.Length; i++)
+            {
+                result[i] = (byte)(first[i] ^ second[i]);
+            }
+            return result;
+        }
+
+        private byte GetFlagByte(bool isCompressed, bool isECMultiplied)
+        {
+            int flag = 0;
+            if (!isECMultiplied)
+                flag |= 0b1100_0000; // two most significat bits
+
+            if (isCompressed)
+                flag |= 0b0010_0000; // bit with value 0x20 indicating compressed public key usage
+
+            // bits with values 0x10 (=0b0001_0000) and 0x08 (=0b0000_1000) are reserved for using multisig
+
+            if (isECMultiplied)
+                flag |= 0b0000_0100; // bit with value 0x04 indicating usage of lot and sequence number (for EC-multiplied keys)
+
+            // Remaining bits are zero and reserved for future versions.
+
+            return (byte)flag;
+        }
+
+
+
+        private bool isDisposed = false;
+
+        /// <summary>
+        /// Releases the resources used by the <see cref="BIP0038"/> class.
+        /// </summary>
+        /// <param name="disposing">
+        /// True to release both managed and unmanaged resources; false to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    if (!(scrypt is null))
+                        scrypt.Dispose();
+                    scrypt = null;
+
+                    if (!(aes is null))
+                        aes.Dispose();
+                    aes = null;
+
+                    if (!(hash is null))
+                        hash.Dispose();
+                    hash = null;
+                }
+
+                isDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by the current instance of the <see cref="BIP0038"/> class.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
---
+++ Src/Tests/Bitcoin/ImprovementProposals/BIP0038Tests.cs
@@ -0,0 +1,148 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Cryptography.Asymmetric.KeyPairs;
+using Autarkysoft.Bitcoin.ImprovementProposals;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.Bitcoin.ImprovementProposals
+{
+    public class BIP0038Tests
+    {
+        public static IEnumerable<object[]> GetEncryptCases()
+        {
+            yield return new object[]
+            {
+                new PrivateKey(Helper.HexToBytes("cbf4b9f70470856bb4f40f80b87edb90865997ffee6df315ab166d713af433a5")),
+                "TestingOneTwoThree",
+                "6PYNKZ1EAgYgmQfmNVamxyXVWHzK5s6DGhwP4J5o44cvXdoY7sRzhtpUeo",
+                "6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg"
+            };
+            yield return new object[]
+            {
+                new PrivateKey(Helper.HexToBytes("09c2686880095b1a4c249ee3ac4eea8a014f11e6f986d0b5025ac1f39afbd9ae")),
+                "Satoshi",
+                "6PYLtMnXvfG3oJde97zRyLYFZCYizPU5T3LwgdYJz1fRhh16bU7u6PPmY7",
+                "6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq"
+            };
+            yield return new object[]
+            {
+                new PrivateKey(Helper.HexToBytes("64eeab5f9be2a01a8365a579511eb3373c87c40da6d2a25f05bda68fe077b66e")),
+                "\u03D2\u0301\u0000\U00010400\U0001F4A9",
+                "6PYVUZ9Y3Q75k52AdHHwGXgW39VvEEgey5N1ziNHWH9xowCxT76fQnnf6P",
+                "6PRW5o9FLp4gJDDVqJQKJFTpMvdsSGJxMYHtHaQBF3ooa8mwD69bapcDQn"
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(GetEncryptCases))]
+        public void DecryptTest(PrivateKey key, string pass, string encryptedComp, string encryptedUnComp)
+        {
+            using BIP0038 bip = new BIP0038();
+            PrivateKey actualKey = bip.Decrypt(encryptedComp, pass, out bool isComp1);
+            PrivateKey actualKey2 = bip.Decrypt(encryptedUnComp, pass, out bool isComp2);
+
+            Assert.True(isComp1);
+            Assert.False(isComp2);
+
+            byte[] expected = key.ToBytes();
+            Assert.Equal(expected, actualKey.ToBytes());
+            Assert.Equal(expected, actualKey2.ToBytes());
+        }
+
+        [Fact]
+        public void Decrypt_NullExceptionTest()
+        {
+            using BIP0038 bip = new BIP0038();
+            string nstr = null;
+            byte[] nba = null;
+
+            Assert.Throws<ArgumentNullException>(() => bip.Decrypt(null, "", out bool isComp));
+            Assert.Throws<ArgumentNullException>(() => bip.Decrypt(" ", "", out bool isComp));
+            Assert.Throws<ArgumentNullException>(() => bip.Decrypt(null, new byte[1], out bool isComp));
+            Assert.Throws<ArgumentNullException>(() => bip.Decrypt(" ", new byte[1], out bool isComp));
+            Assert.Throws<ArgumentNullException>(() => bip.Decrypt("aaa", nstr, out bool isComp));
+            Assert.Throws<ArgumentNullException>(() => bip.Decrypt("aaa", nba, out bool isComp));
+        }
+
+        [Fact]
+        public void Decrypt_DisposedExceptionTest()
+        {
+            BIP0038 bip = new BIP0038();
+            bip.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => bip.Decrypt("aa", "", out bool isComp));
+        }
+
+        [Theory]
+        [InlineData("$", "Input is not a valid base-58 encoded string.")]
+        [InlineData("142viJrTYHA4TzryiEiuQkYk4Ay5TfpzqW", "Invalid encrypted bytes length.")]
+        [InlineData("6Mc5gZg3pNQNMsnHDmZeRfhL1QnC24yBd1VERr3HSnKap5x2wcxYaJivvW", "Invalid prefix.")] // 0x0140
+        [InlineData("6PRW5o9FLp4gJDDVqJQKJFTpMvdsSGJxMYHtHaQBF3ooa8mwD69bapcDQn", "Wrong password (derived address hash is not the same).")]
+        public void Decrypt_FormatExceptionTest(string encrypted, string expError)
+        {
+            using BIP0038 bip = new BIP0038();
+            Exception ex = Assert.Throws<FormatException>(() => bip.Decrypt(encrypted, "wrong pass!", out _));
+            Assert.Contains(expError, ex.Message);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(GetEncryptCases))]
+        public void EncryptTest(PrivateKey key, string pass, string expectedComp, string expectedUncomp)
+        {
+            using BIP0038 bip = new BIP0038();
+            string actualComp = bip.Encrypt(key, pass, true);
+            string actualUncomp = bip.Encrypt(key, pass, false);
+
+            Assert.Equal(expectedComp, actualComp);
+            Assert.Equal(expectedUncomp, actualUncomp);
+        }
+
+        [Fact]
+        public void Encrypt_String_ExceptionTest()
+        {
+            BIP0038 bip = new BIP0038();
+            PrivateKey k = KeyHelper.Prv1;
+            string nstr = null;
+
+            Exception ex = Assert.Throws<ArgumentNullException>(() => bip.Encrypt(null, "a", true));
+            Assert.Contains("Private key can not be null.", ex.Message);
+
+            ex = Assert.Throws<ArgumentNullException>(() => bip.Encrypt(k, nstr, true));
+            Assert.Contains("Password can not be null or empty.", ex.Message);
+
+            ex = Assert.Throws<ArgumentNullException>(() => bip.Encrypt(k, "", true));
+            Assert.Contains("Password can not be null or empty.", ex.Message);
+
+            bip.Dispose();
+            ex = Assert.Throws<ObjectDisposedException>(() => bip.Encrypt(k, "a", true));
+            Assert.Contains("Instance was disposed.", ex.Message);
+        }
+
+        [Fact]
+        public void Encrypt_Bytes_ExceptionTest()
+        {
+            BIP0038 bip = new BIP0038();
+            PrivateKey k = KeyHelper.Prv1;
+            byte[] nba = null;
+
+            Exception ex = Assert.Throws<ArgumentNullException>(() => bip.Encrypt(null, new byte[1], true));
+            Assert.Contains("Private key can not be null.", ex.Message);
+
+            ex = Assert.Throws<ArgumentNullException>(() => bip.Encrypt(k, nba, true));
+            Assert.Contains("Password can not be null or empty.", ex.Message);
+
+            ex = Assert.Throws<ArgumentNullException>(() => bip.Encrypt(k, new byte[0], true));
+            Assert.Contains("Password can not be null or empty.", ex.Message);
+
+            bip.Dispose();
+            ex = Assert.Throws<ObjectDisposedException>(() => bip.Encrypt(k, new byte[1], true));
+            Assert.Contains("Instance was disposed.", ex.Message);
+        }
+    }
+}
