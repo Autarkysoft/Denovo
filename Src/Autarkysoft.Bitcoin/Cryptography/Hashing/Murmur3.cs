8f904e984b641e2f7427be4719c6a210a5509de4
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-05-09 08:50:55 +0430

    Add Murmur3 hash + tests

---
+++ Src/Autarkysoft.Bitcoin/Cryptography/Hashing/Murmur3.cs
@@ -0,0 +1,105 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+
+namespace Autarkysoft.Bitcoin.Cryptography.Hashing
+{
+    /// <summary>
+    /// Implementation of version 3 of the non-cryptographic hash function called MurmurHash
+    /// </summary>
+    public class Murmur3
+    {
+        /// <summary>
+        /// Computes 32-bit hash of the given data.
+        /// </summary>
+        /// <param name="data">Data to hash</param>
+        /// <param name="seed">Seed to use</param>
+        /// <returns>32-bit unsigned integer (hash result)</returns>
+        public unsafe uint ComputeHash32(byte[] data, uint seed)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data), "Data can not be null.");
+
+
+            if (data.Length == 0)
+            {
+                uint res = seed ^ (uint)data.Length;
+                res ^= res >> 16;
+                res *= 0x85ebca6b;
+                res ^= res >> 13;
+                res *= 0xc2b2ae35;
+                res ^= res >> 16;
+
+                return res;
+            }
+
+            uint hash = seed;
+            fixed (byte* dPt = &data[0])
+            {
+                int rem = data.Length & 3; /*=(data.Length % 4)*/
+                for (int i = 0; i < data.Length - rem; i += 4)
+                {
+                    uint k1 = (uint)(dPt[i] | (dPt[i + 1] << 8) | (dPt[i + 2] << 16) | (dPt[i + 3] << 24));
+
+                    k1 *= 0xcc9e2d51;
+                    k1 = ((k1 << 15) | (k1 >> 17));
+                    k1 *= 0x1b873593;
+
+                    hash ^= k1;
+                    hash = (hash << 13) | (hash >> 19);
+                    hash = (hash * 5) + 0xe6546b64;
+                }
+
+                if (rem == 0)
+                {
+                    hash ^= (uint)data.Length;
+                    hash ^= hash >> 16;
+                    hash *= 0x85ebca6b;
+                    hash ^= hash >> 13;
+                    hash *= 0xc2b2ae35;
+                    hash ^= hash >> 16;
+
+                    return hash;
+                }
+                else
+                {
+                    uint k2 = 0;
+                    switch (rem)
+                    {
+                        case 3:
+                            k2 = (uint)(dPt[data.Length - 3] |
+                                       (dPt[data.Length - 2] << 8) |
+                                       (dPt[data.Length - 1] << 16));
+                            break;
+                        case 2:
+                            k2 = (uint)(dPt[data.Length - 2] |
+                                       (dPt[data.Length - 1] << 8));
+                            break;
+                        case 1:
+                            k2 = dPt[data.Length - 1];
+                            break;
+                    }
+
+                    k2 *= 0xcc9e2d51;
+                    k2 = (k2 << 15) | (k2 >> 17);
+                    k2 *= 0x1b873593;
+
+                    hash ^= k2;
+
+
+                    hash ^= (uint)data.Length;
+                    hash ^= hash >> 16;
+                    hash *= 0x85ebca6b;
+                    hash ^= hash >> 13;
+                    hash *= 0xc2b2ae35;
+                    hash ^= hash >> 16;
+
+                    return hash;
+                }
+            }
+        }
+    }
+}
---
+++ Src/Tests/Bitcoin/Cryptography/Hashing/Murmur3Tests.cs
@@ -0,0 +1,75 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Cryptography.Hashing;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests.Bitcoin.Cryptography.Hashing
+{
+    public class Murmur3Tests
+    {
+        public static IEnumerable<object[]> GetHash32Cases()
+        {
+            // https://stackoverflow.com/a/31929528/10401748
+            yield return new object[] { new byte[0], 0, 0 };
+            yield return new object[] { new byte[0], 1, 0x514e28b7 };
+            yield return new object[] { new byte[0], 0xffffffff, 0x81f16f39 };
+            yield return new object[] { new byte[4] { 255, 255, 255, 255 }, 0, 0x76293b50 };
+            yield return new object[] { new byte[4] { 0x21, 0x43, 0x65, 0x87 }, 0, 0xf55b516b };
+            yield return new object[] { new byte[4] { 0x21, 0x43, 0x65, 0x87 }, 0x5082edee, 0x2362f9de };
+            yield return new object[] { new byte[3] { 0x21, 0x43, 0x65 }, 0, 0x7e4a8634 };
+            yield return new object[] { new byte[2] { 0x21, 0x43 }, 0, 0xa0f7b07a };
+            yield return new object[] { new byte[1] { 0x21 }, 0, 0x72661cf4 };
+            yield return new object[] { new byte[4], 0, 0x2362f9de };
+            yield return new object[] { new byte[3], 0, 0x85f0b427 };
+            yield return new object[] { new byte[2], 0, 0x30f4c306 };
+            yield return new object[] { new byte[1], 0, 0x514e28b7 };
+            yield return new object[] { Encoding.UTF8.GetBytes("aaaa"), 0x9747b28c, 0x5a97808a };
+            yield return new object[] { Encoding.UTF8.GetBytes("aaa"), 0x9747b28c, 0x283e0130 };
+            yield return new object[] { Encoding.UTF8.GetBytes("aa"), 0x9747b28c, 0x5d211726 };
+            yield return new object[] { Encoding.UTF8.GetBytes("a"), 0x9747b28c, 0x7fa09ea6 };
+            yield return new object[] { Encoding.UTF8.GetBytes("abcd"), 0x9747b28c, 0xf0478627 };
+            yield return new object[] { Encoding.UTF8.GetBytes("abc"), 0x9747b28c, 0xc84a62dd };
+            yield return new object[] { Encoding.UTF8.GetBytes("ab"), 0x9747b28c, 0x74875592 };
+            yield return new object[] { Encoding.UTF8.GetBytes("Hello, world!"), 0x9747b28c, 0x24884cba };
+            yield return new object[] { Encoding.UTF8.GetBytes(new string('a', 256)), 0x9747b28c, 0x37405bdc };
+            yield return new object[] { Encoding.UTF8.GetBytes("abc"), 0, 0xb3dd93fa };
+            yield return new object[]
+            {
+                Encoding.UTF8.GetBytes("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"), 0, 0xee925b90
+            };
+            yield return new object[]
+            {
+                Encoding.UTF8.GetBytes("The quick brown fox jumps over the lazy dog"), 0x9747b28c, 0x2fa826cd
+            };
+
+            // https://github.com/bitcoin/bitcoin/blob/376294cde6b1588cb17055d8fde567eaf5848c3c/src/test/hash_tests.cpp#L29-L43
+            yield return new object[] { new byte[0], 0xfba4c795, 0x6a396f08U };
+            yield return new object[] { new byte[0], 0xffffffff, 0x81f16f39U };
+            yield return new object[] { new byte[1], 0, 0x514e28b7U };
+            yield return new object[] { new byte[1], 0xfba4c795, 0xea3f0b17U };
+            yield return new object[] { new byte[] { 255 }, 0, 0xfd6cf10dU };
+            yield return new object[] { new byte[] { 0, 0x11 }, 0, 0x16c6b7abU };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22 }, 0, 0x8eb51c3dU };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22, 0x33 }, 0, 0xb4471bf8U };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22, 0x33, 0x44 }, 0, 0xe2301fa8U };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22, 0x33, 0x44, 0x55 }, 0, 0xfc2e4a15U };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 }, 0, 0xb074502cU };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 }, 0, 0x8034d2a0U };
+            yield return new object[] { new byte[] { 0, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 }, 0, 0xb4698defU };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetHash32Cases))]
+        public void ComputeHash32Test(byte[] data, uint seed, uint expected)
+        {
+            Murmur3 hasher = new Murmur3();
+            uint actual = hasher.ComputeHash32(data, seed);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
