48a95814d60ddcbd1605662115035b57743ad254
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-05-14 08:49:33 +0430

    Add specific CodeSeparatorOP tests

--- Src/Tests/Bitcoin/Blockchain/Scripts/Operations/CodeSeparatorOpTests.cs
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/CodeSeparatorOpTests.cs
@@ -28,12 +28,70 @@ namespace Tests.Bitcoin.Blockchain.Scripts.Operations
         }
 
         [Fact]
-        public void WriteToStreamForSigningTest()
+        public void WriteToStreamTest()
         {
+            // This test makes sure WriteToStream is not overriden and performs correctly
             CodeSeparatorOp op = new CodeSeparatorOp();
             FastStream stream = new FastStream(5);
 
+            op.WriteToStream(stream);
+
+            Assert.Equal(new byte[1] { (byte)OP.CodeSeparator }, stream.ToByteArray());
+        }
+
+        [Fact]
+        public void WriteToStreamForSigningTest()
+        {
+            FastStream stream = new FastStream(5);
+
+            CodeSeparatorOp op = new CodeSeparatorOp();
             op.WriteToStreamForSigning(stream, new byte[] { 1, 2 });
+
+            // Execution should not make any difference
+            op.IsExecuted = true;
+            op.WriteToStreamForSigning(stream, new byte[] { 1, 2 });
+
+            Assert.Equal(new byte[0], stream.ToByteArray());
+        }
+
+        [Fact]
+        public void WriteToStreamForSigning_MultiTest()
+        {
+            FastStream stream = new FastStream(5);
+
+            CodeSeparatorOp op = new CodeSeparatorOp();
+            op.WriteToStreamForSigning(stream, new byte[][] { new byte[] { 1, 2 } });
+
+            // Execution should not make any difference
+            op.IsExecuted = true;
+            op.WriteToStreamForSigning(stream, new byte[][] { new byte[] { 1, 2 } });
+
+            Assert.Equal(new byte[0], stream.ToByteArray());
+        }
+
+        [Fact]
+        public void WriteToStreamForSigningSegWit_NotExecutedTest()
+        {
+            CodeSeparatorOp op = new CodeSeparatorOp()
+            {
+                IsExecuted = false
+            };
+            FastStream stream = new FastStream(5);
+
+            op.WriteToStreamForSigningSegWit(stream);
+
+            Assert.Equal(new byte[1] { (byte)OP.CodeSeparator }, stream.ToByteArray());
+        }
+        [Fact]
+        public void WriteToStreamForSigningSegWit_ExecutedTest()
+        {
+            CodeSeparatorOp op = new CodeSeparatorOp()
+            {
+                IsExecuted = true
+            };
+            FastStream stream = new FastStream(5);
+
+            op.WriteToStreamForSigningSegWit(stream);
 
             Assert.Equal(new byte[0], stream.ToByteArray());
         }
