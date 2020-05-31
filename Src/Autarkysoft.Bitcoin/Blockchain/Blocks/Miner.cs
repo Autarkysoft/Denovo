e0bd4a3d728fcbc046cabc554d2eb5afc4aaae01
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-05-31 11:47:04 +0430

    Add a simple miner + test

---
+++ Src/Autarkysoft.Bitcoin/Blockchain/Blocks/Miner.cs
@@ -0,0 +1,328 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Cryptography.Hashing;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Autarkysoft.Bitcoin.Blockchain.Blocks
+{
+    /// <summary>
+    /// A block miner that finds and sets correct variable so that the resulting block header hash is lower than
+    /// the defined target. Implements <see cref="IDisposable"/>.
+    /// </summary>
+    public class Miner : IDisposable
+    {
+        // TODO: this is a primary implementation of the miner. Currently it only goes through all nonces and returns true/false.
+        // It needs more improvement:
+        //      *Implement IMemoryPool first and inject it here so that miner can update transactions, merkle root, time,...
+        //       on its own,
+        //      *Add a CancelationToken so that the caller (which is probably the client with the open socket) can cancel
+        //       when the same block was found by someone else.
+
+        private Sha256 sha = new Sha256();
+
+        private readonly uint[] w = new uint[64];
+
+        private readonly uint[] block1 = new uint[64];
+        private readonly uint[] block2 = new uint[64];
+        private readonly uint[] block3 = new uint[64];
+        private readonly uint[] hashState1 = new uint[8];
+        private readonly uint[] hashState3 = new uint[8];
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private uint SSIG0(uint x) => (x >> 7 | x << 25) ^ (x >> 18 | x << 14) ^ (x >> 3);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private uint SSIG1(uint x) => (x >> 17 | x << 15) ^ (x >> 19 | x << 13) ^ (x >> 10);
+
+
+        /// <summary>
+        /// Goes through all possible nonces to find the appropriate hash. Return value indicates success.
+        /// </summary>
+        /// <param name="block">Block to mine</param>
+        /// <returns>True if a successful hash was found; otherwise false.</returns>
+        public unsafe bool Mine(IBlock block)
+        {
+            /*** Target ***/
+            uint[] targetArr = block.NBits.ToUInt32Array();
+
+            // Compute SHA256(SHA256(80_byte_header))
+            // Convert to integer in little endian order
+            // Finish if hash <= target
+
+            // Double SHA256 of 80 bytes is:
+            //    Compress 64 bytes              -> block1, hashState1
+            //    Compress 16 bytes + pad + len  -> block2, hashState2
+            //    Compress 32 bytes + pad + len  -> block3, hashState3
+
+            fixed (uint* blkPt1 = &block1[0], blkPt2 = &block2[0], blkPt3 = &block3[0])
+            fixed (uint* hPt1 = &hashState1[0], hPt3 = &hashState3[0], wPt = &w[0])
+            fixed (uint* tarPt = &targetArr[0])
+            fixed (byte* prvBlkH = &block.PreviousBlockHeaderHash[0], mrkl = &block.MerkleRootHash[0])
+            {
+                /*** First block (64 bytes) ***/
+                // 4 byte block version
+                blkPt1[0] = ((uint)block.Version).SwapEndian();
+
+                // 32 byte previous block hash
+                blkPt1[1] = (uint)(prvBlkH[0] << 24 | prvBlkH[1] << 16 | prvBlkH[2] << 8 | prvBlkH[3]);
+                blkPt1[2] = (uint)(prvBlkH[4] << 24 | prvBlkH[5] << 16 | prvBlkH[6] << 8 | prvBlkH[7]);
+                blkPt1[3] = (uint)(prvBlkH[8] << 24 | prvBlkH[9] << 16 | prvBlkH[10] << 8 | prvBlkH[11]);
+                blkPt1[4] = (uint)(prvBlkH[12] << 24 | prvBlkH[13] << 16 | prvBlkH[14] << 8 | prvBlkH[15]);
+                blkPt1[5] = (uint)(prvBlkH[16] << 24 | prvBlkH[17] << 16 | prvBlkH[18] << 8 | prvBlkH[19]);
+                blkPt1[6] = (uint)(prvBlkH[20] << 24 | prvBlkH[21] << 16 | prvBlkH[22] << 8 | prvBlkH[23]);
+                blkPt1[7] = (uint)(prvBlkH[24] << 24 | prvBlkH[25] << 16 | prvBlkH[26] << 8 | prvBlkH[27]);
+                blkPt1[8] = (uint)(prvBlkH[28] << 24 | prvBlkH[29] << 16 | prvBlkH[30] << 8 | prvBlkH[31]);
+
+                // 28/32 byte MerkleRoot
+                blkPt1[9] = (uint)(mrkl[0] << 24 | mrkl[1] << 16 | mrkl[2] << 8 | mrkl[3]);
+                blkPt1[10] = (uint)(mrkl[4] << 24 | mrkl[5] << 16 | mrkl[6] << 8 | mrkl[7]);
+                blkPt1[11] = (uint)(mrkl[8] << 24 | mrkl[9] << 16 | mrkl[10] << 8 | mrkl[11]);
+                blkPt1[12] = (uint)(mrkl[12] << 24 | mrkl[13] << 16 | mrkl[14] << 8 | mrkl[15]);
+                blkPt1[13] = (uint)(mrkl[16] << 24 | mrkl[17] << 16 | mrkl[18] << 8 | mrkl[19]);
+                blkPt1[14] = (uint)(mrkl[20] << 24 | mrkl[21] << 16 | mrkl[22] << 8 | mrkl[23]);
+                blkPt1[15] = (uint)(mrkl[24] << 24 | mrkl[25] << 16 | mrkl[26] << 8 | mrkl[27]);
+
+
+                // Compress first block (the result should be reused for all nonces)
+                sha.Init(hPt1);
+                for (int i = 16; i < 64; i++)
+                {
+                    blkPt1[i] = SSIG1(blkPt1[i - 2]) + blkPt1[i - 7] + SSIG0(blkPt1[i - 15]) + blkPt1[i - 16];
+                }
+                sha.CompressBlock_WithWSet(hPt1, blkPt1);
+
+                /*** Second block (16 bytes) ***/
+                // (final 4 bytes) 32/32 byte MerkleRoot
+                blkPt2[0] = (uint)(mrkl[28] << 24 | mrkl[29] << 16 | mrkl[30] << 8 | mrkl[31]);
+
+
+                // 4 byte BlockTime (index at 1)
+                // will be incremented inside the block time loop
+                blkPt2[1] = block.BlockTime.SwapEndian();
+
+                // 4 byte NBits
+                blkPt2[2] = ((uint)block.NBits).SwapEndian();
+
+                // 4 byte Nonce (index at 3)
+                // will be set and incremented inside the nonce loop
+
+                // append length and padding:
+                blkPt2[4] = 0b10000000_00000000_00000000_00000000U;
+                // from 5 to 14 are zeros and are already set
+                blkPt2[15] = 640; // 80*8=640
+
+                // Second block values set up to here won't change in the loop
+
+                /*** Third block, second hash (32 bytes) ***/
+                // Set values that don't change
+                blkPt3[8] = 0b10000000_00000000_00000000_00000000U;
+                // From 9 to 14 are zero, already set and won't change
+                blkPt3[15] = 256; // 32*8=256
+
+                // Nonce loop
+                for (ulong nonce = block.Nonce.SwapEndian(); nonce <= uint.MaxValue; nonce++)
+                {
+                    blkPt2[3] = (uint)nonce;
+
+                    blkPt2[16] = SSIG0(blkPt2[1]) + blkPt2[0];
+                    blkPt2[17] = 17825792 + SSIG0(blkPt2[2]) + blkPt2[1];
+                    blkPt2[18] = SSIG1(blkPt2[16]) + SSIG0(blkPt2[3]) + blkPt2[2];
+                    blkPt2[19] = SSIG1(blkPt2[17]) + 285220864 + blkPt2[3];
+                    blkPt2[20] = SSIG1(blkPt2[18]) + 2147483648;
+                    blkPt2[21] = SSIG1(blkPt2[19]);
+                    blkPt2[22] = SSIG1(blkPt2[20]) + 640;
+                    blkPt2[23] = SSIG1(blkPt2[21]) + blkPt2[16];
+                    blkPt2[24] = SSIG1(blkPt2[22]) + blkPt2[17];
+                    blkPt2[25] = SSIG1(blkPt2[23]) + blkPt2[18];
+                    blkPt2[26] = SSIG1(blkPt2[24]) + blkPt2[19];
+                    blkPt2[27] = SSIG1(blkPt2[25]) + blkPt2[20];
+                    blkPt2[28] = SSIG1(blkPt2[26]) + blkPt2[21];
+                    blkPt2[29] = SSIG1(blkPt2[27]) + blkPt2[22];
+                    blkPt2[30] = SSIG1(blkPt2[28]) + blkPt2[23] + 10485845;
+                    blkPt2[31] = SSIG1(blkPt2[29]) + blkPt2[24] + SSIG0(blkPt2[16]) + 640;
+                    blkPt2[32] = SSIG1(blkPt2[30]) + blkPt2[25] + SSIG0(blkPt2[17]) + blkPt2[16];
+                    blkPt2[33] = SSIG1(blkPt2[31]) + blkPt2[26] + SSIG0(blkPt2[18]) + blkPt2[17];
+                    blkPt2[34] = SSIG1(blkPt2[32]) + blkPt2[27] + SSIG0(blkPt2[19]) + blkPt2[18];
+                    blkPt2[35] = SSIG1(blkPt2[33]) + blkPt2[28] + SSIG0(blkPt2[20]) + blkPt2[19];
+                    blkPt2[36] = SSIG1(blkPt2[34]) + blkPt2[29] + SSIG0(blkPt2[21]) + blkPt2[20];
+                    blkPt2[37] = SSIG1(blkPt2[35]) + blkPt2[30] + SSIG0(blkPt2[22]) + blkPt2[21];
+                    blkPt2[38] = SSIG1(blkPt2[36]) + blkPt2[31] + SSIG0(blkPt2[23]) + blkPt2[22];
+                    blkPt2[39] = SSIG1(blkPt2[37]) + blkPt2[32] + SSIG0(blkPt2[24]) + blkPt2[23];
+                    blkPt2[40] = SSIG1(blkPt2[38]) + blkPt2[33] + SSIG0(blkPt2[25]) + blkPt2[24];
+                    blkPt2[41] = SSIG1(blkPt2[39]) + blkPt2[34] + SSIG0(blkPt2[26]) + blkPt2[25];
+                    blkPt2[42] = SSIG1(blkPt2[40]) + blkPt2[35] + SSIG0(blkPt2[27]) + blkPt2[26];
+                    blkPt2[43] = SSIG1(blkPt2[41]) + blkPt2[36] + SSIG0(blkPt2[28]) + blkPt2[27];
+                    blkPt2[44] = SSIG1(blkPt2[42]) + blkPt2[37] + SSIG0(blkPt2[29]) + blkPt2[28];
+                    blkPt2[45] = SSIG1(blkPt2[43]) + blkPt2[38] + SSIG0(blkPt2[30]) + blkPt2[29];
+                    blkPt2[46] = SSIG1(blkPt2[44]) + blkPt2[39] + SSIG0(blkPt2[31]) + blkPt2[30];
+                    blkPt2[47] = SSIG1(blkPt2[45]) + blkPt2[40] + SSIG0(blkPt2[32]) + blkPt2[31];
+                    blkPt2[48] = SSIG1(blkPt2[46]) + blkPt2[41] + SSIG0(blkPt2[33]) + blkPt2[32];
+                    blkPt2[49] = SSIG1(blkPt2[47]) + blkPt2[42] + SSIG0(blkPt2[34]) + blkPt2[33];
+                    blkPt2[50] = SSIG1(blkPt2[48]) + blkPt2[43] + SSIG0(blkPt2[35]) + blkPt2[34];
+                    blkPt2[51] = SSIG1(blkPt2[49]) + blkPt2[44] + SSIG0(blkPt2[36]) + blkPt2[35];
+                    blkPt2[52] = SSIG1(blkPt2[50]) + blkPt2[45] + SSIG0(blkPt2[37]) + blkPt2[36];
+                    blkPt2[53] = SSIG1(blkPt2[51]) + blkPt2[46] + SSIG0(blkPt2[38]) + blkPt2[37];
+                    blkPt2[54] = SSIG1(blkPt2[52]) + blkPt2[47] + SSIG0(blkPt2[39]) + blkPt2[38];
+                    blkPt2[55] = SSIG1(blkPt2[53]) + blkPt2[48] + SSIG0(blkPt2[40]) + blkPt2[39];
+                    blkPt2[56] = SSIG1(blkPt2[54]) + blkPt2[49] + SSIG0(blkPt2[41]) + blkPt2[40];
+                    blkPt2[57] = SSIG1(blkPt2[55]) + blkPt2[50] + SSIG0(blkPt2[42]) + blkPt2[41];
+                    blkPt2[58] = SSIG1(blkPt2[56]) + blkPt2[51] + SSIG0(blkPt2[43]) + blkPt2[42];
+                    blkPt2[59] = SSIG1(blkPt2[57]) + blkPt2[52] + SSIG0(blkPt2[44]) + blkPt2[43];
+                    blkPt2[60] = SSIG1(blkPt2[58]) + blkPt2[53] + SSIG0(blkPt2[45]) + blkPt2[44];
+                    blkPt2[61] = SSIG1(blkPt2[59]) + blkPt2[54] + SSIG0(blkPt2[46]) + blkPt2[45];
+                    blkPt2[62] = SSIG1(blkPt2[60]) + blkPt2[55] + SSIG0(blkPt2[47]) + blkPt2[46];
+                    blkPt2[63] = SSIG1(blkPt2[61]) + blkPt2[56] + SSIG0(blkPt2[48]) + blkPt2[47];
+
+                    // HashState is the first block's hashState but it should remain the same for all nonces.
+                    // After compressing this block hashState should be used as the next "block".
+                    // So copy first block's hashState in third block
+                    Buffer.MemoryCopy(hPt1, blkPt3, 32, 32);
+
+                    // Compress second block
+                    sha.CompressBlock_WithWSet(blkPt3, blkPt2);
+
+                    // Compress third block
+                    sha.Init(hPt3);
+
+                    blkPt3[16] = SSIG0(blkPt3[1]) + blkPt3[0];
+                    blkPt3[17] = 10485760 + SSIG0(blkPt3[2]) + blkPt3[1];
+                    blkPt3[18] = SSIG1(blkPt3[16]) + SSIG0(blkPt3[3]) + blkPt3[2];
+                    blkPt3[19] = SSIG1(blkPt3[17]) + SSIG0(blkPt3[4]) + blkPt3[3];
+                    blkPt3[20] = SSIG1(blkPt3[18]) + SSIG0(blkPt3[5]) + blkPt3[4];
+                    blkPt3[21] = SSIG1(blkPt3[19]) + SSIG0(blkPt3[6]) + blkPt3[5];
+                    blkPt3[22] = SSIG1(blkPt3[20]) + 256 + SSIG0(blkPt3[7]) + blkPt3[6];
+                    blkPt3[23] = SSIG1(blkPt3[21]) + blkPt3[16] + 285220864 + blkPt3[7];
+                    blkPt3[24] = SSIG1(blkPt3[22]) + blkPt3[17] + 2147483648;
+                    blkPt3[25] = SSIG1(blkPt3[23]) + blkPt3[18];
+                    blkPt3[26] = SSIG1(blkPt3[24]) + blkPt3[19];
+                    blkPt3[27] = SSIG1(blkPt3[25]) + blkPt3[20];
+                    blkPt3[28] = SSIG1(blkPt3[26]) + blkPt3[21];
+                    blkPt3[29] = SSIG1(blkPt3[27]) + blkPt3[22];
+                    blkPt3[30] = SSIG1(blkPt3[28]) + blkPt3[23] + 4194338;
+                    blkPt3[31] = SSIG1(blkPt3[29]) + blkPt3[24] + SSIG0(blkPt3[16]) + 256;
+                    blkPt3[32] = SSIG1(blkPt3[30]) + blkPt3[25] + SSIG0(blkPt3[17]) + blkPt3[16];
+                    blkPt3[33] = SSIG1(blkPt3[31]) + blkPt3[26] + SSIG0(blkPt3[18]) + blkPt3[17];
+                    blkPt3[34] = SSIG1(blkPt3[32]) + blkPt3[27] + SSIG0(blkPt3[19]) + blkPt3[18];
+                    blkPt3[35] = SSIG1(blkPt3[33]) + blkPt3[28] + SSIG0(blkPt3[20]) + blkPt3[19];
+                    blkPt3[36] = SSIG1(blkPt3[34]) + blkPt3[29] + SSIG0(blkPt3[21]) + blkPt3[20];
+                    blkPt3[37] = SSIG1(blkPt3[35]) + blkPt3[30] + SSIG0(blkPt3[22]) + blkPt3[21];
+                    blkPt3[38] = SSIG1(blkPt3[36]) + blkPt3[31] + SSIG0(blkPt3[23]) + blkPt3[22];
+                    blkPt3[39] = SSIG1(blkPt3[37]) + blkPt3[32] + SSIG0(blkPt3[24]) + blkPt3[23];
+                    blkPt3[40] = SSIG1(blkPt3[38]) + blkPt3[33] + SSIG0(blkPt3[25]) + blkPt3[24];
+                    blkPt3[41] = SSIG1(blkPt3[39]) + blkPt3[34] + SSIG0(blkPt3[26]) + blkPt3[25];
+                    blkPt3[42] = SSIG1(blkPt3[40]) + blkPt3[35] + SSIG0(blkPt3[27]) + blkPt3[26];
+                    blkPt3[43] = SSIG1(blkPt3[41]) + blkPt3[36] + SSIG0(blkPt3[28]) + blkPt3[27];
+                    blkPt3[44] = SSIG1(blkPt3[42]) + blkPt3[37] + SSIG0(blkPt3[29]) + blkPt3[28];
+                    blkPt3[45] = SSIG1(blkPt3[43]) + blkPt3[38] + SSIG0(blkPt3[30]) + blkPt3[29];
+                    blkPt3[46] = SSIG1(blkPt3[44]) + blkPt3[39] + SSIG0(blkPt3[31]) + blkPt3[30];
+                    blkPt3[47] = SSIG1(blkPt3[45]) + blkPt3[40] + SSIG0(blkPt3[32]) + blkPt3[31];
+                    blkPt3[48] = SSIG1(blkPt3[46]) + blkPt3[41] + SSIG0(blkPt3[33]) + blkPt3[32];
+                    blkPt3[49] = SSIG1(blkPt3[47]) + blkPt3[42] + SSIG0(blkPt3[34]) + blkPt3[33];
+                    blkPt3[50] = SSIG1(blkPt3[48]) + blkPt3[43] + SSIG0(blkPt3[35]) + blkPt3[34];
+                    blkPt3[51] = SSIG1(blkPt3[49]) + blkPt3[44] + SSIG0(blkPt3[36]) + blkPt3[35];
+                    blkPt3[52] = SSIG1(blkPt3[50]) + blkPt3[45] + SSIG0(blkPt3[37]) + blkPt3[36];
+                    blkPt3[53] = SSIG1(blkPt3[51]) + blkPt3[46] + SSIG0(blkPt3[38]) + blkPt3[37];
+                    blkPt3[54] = SSIG1(blkPt3[52]) + blkPt3[47] + SSIG0(blkPt3[39]) + blkPt3[38];
+                    blkPt3[55] = SSIG1(blkPt3[53]) + blkPt3[48] + SSIG0(blkPt3[40]) + blkPt3[39];
+                    blkPt3[56] = SSIG1(blkPt3[54]) + blkPt3[49] + SSIG0(blkPt3[41]) + blkPt3[40];
+                    blkPt3[57] = SSIG1(blkPt3[55]) + blkPt3[50] + SSIG0(blkPt3[42]) + blkPt3[41];
+                    blkPt3[58] = SSIG1(blkPt3[56]) + blkPt3[51] + SSIG0(blkPt3[43]) + blkPt3[42];
+                    blkPt3[59] = SSIG1(blkPt3[57]) + blkPt3[52] + SSIG0(blkPt3[44]) + blkPt3[43];
+                    blkPt3[60] = SSIG1(blkPt3[58]) + blkPt3[53] + SSIG0(blkPt3[45]) + blkPt3[44];
+                    blkPt3[61] = SSIG1(blkPt3[59]) + blkPt3[54] + SSIG0(blkPt3[46]) + blkPt3[45];
+                    blkPt3[62] = SSIG1(blkPt3[60]) + blkPt3[55] + SSIG0(blkPt3[47]) + blkPt3[46];
+                    blkPt3[63] = SSIG1(blkPt3[61]) + blkPt3[56] + SSIG0(blkPt3[48]) + blkPt3[47];
+
+                    sha.CompressBlock_WithWSet(hPt3, blkPt3);
+
+                    // Check to see if the hash result is smaller than target
+                    if (CompareTarget(hPt3, tarPt, hashState3.Length))
+                    {
+                        block.Nonce = ((uint)nonce).SwapEndian();
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        private unsafe bool CompareTarget(uint* hash, uint* target, int length)
+        {
+            for (int i = 0, j = 7; i < 8; i++, j--)
+            {
+                if (target[i] == 0 && hash[j] != 0)
+                {
+                    return false;
+                }
+                else if (target[i] == 0 && hash[j] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    uint h = hash[j].SwapEndian();
+                    if (h > target[i])
+                    {
+                        return false;
+                    }
+                    else if (h < target[i])
+                    {
+                        return true;
+                    }
+                    else if (i + 1 < 8 && target[i + 1] != 0) // && h == target[i]
+                    {
+                        h = hash[i - 1].SwapEndian();
+                        if (h > target[i + 1])
+                        {
+                            return false;
+                        }
+                        else if (h < target[i + 1])
+                        {
+                            return true;
+                        }
+                        // Target will never have more than 2 set values in it (the rest are zero)
+                    }
+                    // else is skipped
+                }
+            }
+
+            return false;
+        }
+
+
+
+        private bool isDisposed = false;
+
+        /// <summary>
+        /// Releases the resources used by the <see cref="Miner"/> class.
+        /// </summary>
+        /// <param name="disposing">
+        /// True to release both managed and unmanaged resources; false to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    if (!(sha is null))
+                        sha.Dispose();
+                    sha = null;
+                }
+
+                isDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by the current instance of the <see cref="Miner"/> class.
+        /// </summary>
+        public void Dispose() => Dispose(true);
+    }
+}
---
+++ Src/Tests/Bitcoin/Blockchain/Blocks/MinerTest.cs
@@ -0,0 +1,32 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin;
+using Autarkysoft.Bitcoin.Blockchain.Blocks;
+using Autarkysoft.Bitcoin.Encoders;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Blocks
+{
+    public class MinerTest
+    {
+        [Fact]
+        public void MineTest()
+        {
+            Block blk = new Block();
+            // TestNet block #1,670,926
+            blk.TryDeserializeHeader(new FastStreamReader(Base16.Decode("00e0ff3ff79fa236e509c35d006c58546db4f27c4874e6dfa4dd5b30b01b1b000000000034310adae6b8d3cca58e56a42eb55ab3c17599cc696b133afe0e4c2c49ecfa2cb5b1795eb334011aabf54a10")), out _);
+            uint expected = 424342955;
+            Assert.NotEqual(expected, blk.Nonce);
+
+            using Miner miner = new Miner();
+
+            bool success = miner.Mine(blk);
+            Assert.True(success);
+            Assert.Equal(expected, blk.Nonce);
+            Assert.Equal("000000000000005befb0b49dec35738f6ad493f9c7a6e777d69836323f1cf5f2", blk.GetBlockID());
+        }
+    }
+}
