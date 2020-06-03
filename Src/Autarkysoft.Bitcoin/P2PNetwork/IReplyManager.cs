9c5bacfd56bc61d210bc9e44271ba8ddc40b825e
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-06-03 13:38:57 +0430

    Add a reply manager and the interface for migration

---
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/IReplyManager.cs
@@ -0,0 +1,32 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.P2PNetwork.Messages;
+using Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads;
+
+namespace Autarkysoft.Bitcoin.P2PNetwork
+{
+    /// <summary>
+    /// Defines methods that a P2P message reply manager implements.
+    /// </summary>
+    public interface IReplyManager
+    {
+        /// <summary>
+        /// Builds and returns a new <see cref="Message"/> with a <see cref="RejectPayload"/>.
+        /// </summary>
+        /// <param name="plt">Message type being rejected</param>
+        /// <param name="error">Error message (reason for rejection)</param>
+        /// <returns>A new reject message</returns>
+        Message GetReject(PayloadType plt, string error);
+
+        /// <summary>
+        /// Builds and returns the appropriate response to the given <see cref="Message"/>.
+        /// In case there is no response null should be returned.
+        /// </summary>
+        /// <param name="msg"><see cref="Message"/> to reply to</param>
+        /// <returns>Response <see cref="Message"/> or null</returns>
+        Message GetReply(Message msg);
+    }
+}
---
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/ReplyManager.cs
@@ -0,0 +1,53 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.P2PNetwork.Messages;
+using Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads;
+
+namespace Autarkysoft.Bitcoin.P2PNetwork
+{
+    /// <summary>
+    /// Implementation of a reply manager to handle creation of new <see cref="Message"/>s to return in response to
+    /// received <see cref="Message"/>s.
+    /// Implements <see cref="IReplyManager"/>.
+    /// </summary>
+    public class ReplyManager : IReplyManager
+    {
+        private readonly NetworkType netType;
+
+        /// <inheritdoc/>
+        public Message GetReject(PayloadType plt, string error)
+        {
+            return new Message(netType)
+            {
+                Payload = new RejectPayload()
+                {
+                    Code = RejectCode.FailedToDecodeMessage,
+                    RejectedMessage = plt,
+                    Reason = error
+                }
+            };
+        }
+
+        /// <inheritdoc/>
+        public Message GetReply(Message msg)
+        {
+            if (!(msg.Payload is null))
+            {
+                // TODO: add all cases
+                if (msg.Payload is PingPayload ping)
+                {
+                    return new Message(new PongPayload(ping.Nonce), netType);
+                }
+                else if (msg.Payload is VersionPayload)
+                {
+                    return new Message(new VerackPayload(), netType);
+                }
+            }
+
+            return null;
+        }
+    }
+}
