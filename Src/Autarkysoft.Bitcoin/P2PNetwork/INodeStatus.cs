b863d3d0f7cfab08062b16054d0c6da92a75f8f4
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-06-18 10:11:29 +0430

    Add node status

---
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/INodeStatus.cs
@@ -0,0 +1,22 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+
+namespace Autarkysoft.Bitcoin.P2PNetwork
+{
+    public interface INodeStatus
+    {
+        bool SendCompact { get; set; }
+        bool ShouldDisconnect { get; }
+        DateTime LastSeen { get; }
+
+
+        void UpdateTime();
+        void AddSmallViolation();
+        void AddMediumViolation();
+        void AddBigViolation();
+    }
+}
---
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/NodeStatus.cs
@@ -0,0 +1,35 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+
+namespace Autarkysoft.Bitcoin.P2PNetwork
+{
+    public class NodeStatus : INodeStatus
+    {
+        private int violation;
+        private const int SmallV = 10;
+        private const int MediumV = 20;
+        private const int BigV = 50;
+        private const int DisconnectThreshold = 100;
+
+
+        /// <inheritdoc/>
+        public bool SendCompact { get; set; }
+        /// <inheritdoc/>
+        public bool ShouldDisconnect => violation > DisconnectThreshold;
+        /// <inheritdoc/>
+        public DateTime LastSeen { get; private set; }
+
+        /// <inheritdoc/>
+        public void UpdateTime() => LastSeen = DateTime.Now;
+        /// <inheritdoc/>
+        public void AddBigViolation() => violation += SmallV;
+        /// <inheritdoc/>
+        public void AddMediumViolation() => violation += MediumV;
+        /// <inheritdoc/>
+        public void AddSmallViolation() => violation += BigV;
+    }
+}
