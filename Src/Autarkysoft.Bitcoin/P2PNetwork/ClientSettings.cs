580f065418f2e7f937dee1abc786ab7a48e92cbd
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-07-13 14:03:56 +0430

    Introducing ClientSettings to be used by the "client" and passed as dependency to each Node

---
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/ClientSettings.cs
@@ -0,0 +1,77 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Encoders;
+using Autarkysoft.Bitcoin.ImprovementProposals;
+using Autarkysoft.Bitcoin.P2PNetwork.Messages;
+using System.Reflection;
+
+namespace Autarkysoft.Bitcoin.P2PNetwork
+{
+    /// <summary>
+    /// Defines client settings. 1 instance should be used by the client and passed to each node instance.
+    /// Implements <see cref="IClientSettings"/>.
+    /// </summary>
+    public class ClientSettings : IClientSettings
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ClientSettings"/> with default parameters.
+        /// </summary>
+        public ClientSettings()
+            : this(Constants.P2PProtocolVersion,
+                   true,
+                   new BIP0014("Bitcoin.Net", Assembly.GetExecutingAssembly().GetName().Version, "Bitcoin from scratch").ToString(),
+                   NetworkType.MainNet,
+                   NodeServiceFlags.All)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ClientSettings"/> with the given parameters.
+        /// </summary>
+        /// <param name="pver">Protocol version</param>
+        /// <param name="relay">True to relay blocks and transactions; false otherwise</param>
+        /// <param name="ua">User agent</param>
+        /// <param name="netType">Network type</param>
+        /// <param name="servs">Services supported by this node</param>
+        public ClientSettings(int pver, bool relay, BIP0014 ua, NetworkType netType, NodeServiceFlags servs)
+            : this(pver, relay, ua.ToString(), netType, servs)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ClientSettings"/> with the given parameters.
+        /// </summary>
+        /// <param name="pver">Protocol version</param>
+        /// <param name="relay">True to relay blocks and transactions; false otherwise</param>
+        /// <param name="ua">User agent as defined by <see cref="BIP0014"/></param>
+        /// <param name="netType">Network type</param>
+        /// <param name="servs">Services supported by this node</param>
+        public ClientSettings(int pver, bool relay, string ua, NetworkType netType, NodeServiceFlags servs)
+        {
+            ProtocolVersion = pver;
+            Relay = relay;
+            UserAgent = ua;
+            Network = netType;
+            Services = servs;
+        }
+
+
+        /// <inheritdoc/>
+        public int ProtocolVersion { get; set; }
+        /// <inheritdoc/>
+        public bool Relay { get; set; }
+        /// <inheritdoc/>
+        public string UserAgent { get; set; }
+        /// <inheritdoc/>
+        public NetworkType Network { get; set; }
+        /// <inheritdoc/>
+        public NodeServiceFlags Services { get; set; }
+        /// <inheritdoc/>
+        public long Time => UnixTimeStamp.GetEpochUtcNow();
+        /// <inheritdoc/>
+        public ushort Port { get; set; }
+    }
+}
---
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/IClientSettings.cs
@@ -0,0 +1,44 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.P2PNetwork.Messages;
+
+namespace Autarkysoft.Bitcoin.P2PNetwork
+{
+    /// <summary>
+    /// Defines methods and properties of a client and is used by all <see cref="Node"/> instances.
+    /// </summary>
+    public interface IClientSettings
+    {
+        /// <summary>
+        /// Protocol version that the client supports
+        /// </summary>
+        int ProtocolVersion { get; set; }
+        /// <summary>
+        /// Returns whether the client will relay blocks and transactions or not
+        /// </summary>
+        bool Relay { get; set; }
+        /// <summary>
+        /// Name of the client as defined by <see cref="ImprovementProposals.BIP0014"/>
+        /// </summary>
+        string UserAgent { get; set; }
+        /// <summary>
+        /// Network type
+        /// </summary>
+        NetworkType Network { get; set; }
+        /// <summary>
+        /// Services this client supports
+        /// </summary>
+        NodeServiceFlags Services { get; set; }
+        /// <summary>
+        /// Returns the current UTC time as an epoch timestamp
+        /// </summary>
+        long Time { get; }
+        /// <summary>
+        /// Port that this client listens to and makes connection over
+        /// </summary>
+        ushort Port { get; set; }
+    }
+}
