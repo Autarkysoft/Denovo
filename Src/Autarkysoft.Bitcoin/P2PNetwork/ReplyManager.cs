52e680761049bfa6fe387bb81febc7e74dcc075a
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-08-02 09:11:58 +0430

    Improve ReplyManager way of handling handshake process

--- Src/Autarkysoft.Bitcoin/P2PNetwork/ReplyManager.cs
+++ Src/Autarkysoft.Bitcoin/P2PNetwork/ReplyManager.cs
@@ -4,6 +4,7 @@
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
 using Autarkysoft.Bitcoin.Blockchain;
+using Autarkysoft.Bitcoin.Cryptography;
 using Autarkysoft.Bitcoin.P2PNetwork.Messages;
 using Autarkysoft.Bitcoin.P2PNetwork.Messages.MessagePayloads;
 using System;
@@ -36,30 +37,32 @@ namespace Autarkysoft.Bitcoin.P2PNetwork
         private readonly INodeStatus nodeStatus;
         private readonly IClientSettings settings;
         private readonly IBlockchain blockchain;
+        /// <summary>
+        /// A weak RNG to generate nonces for messages.
+        /// </summary>
+        public IRandomNonceGenerator rng = new RandomNonceGenerator();
 
         /// <inheritdoc/>
         public Message GetVersionMsg() => GetVersionMsg(new NetworkAddress(settings.Services, IPAddress.Loopback, settings.Port));
 
         /// <inheritdoc/>
-        public Message GetVersionMsg(NetworkAddress addr)
+        public Message GetVersionMsg(NetworkAddress recvAddr)
         {
-            byte[] temp = new byte[8];
-            new Random().NextBytes(temp);
-
             var ver = new VersionPayload()
             {
                 Version = settings.ProtocolVersion,
                 Services = settings.Services,
                 Timestamp = settings.Time,
-                ReceivingNodeNetworkAddress = addr,
+                ReceivingNodeNetworkAddress = recvAddr,
                 TransmittingNodeNetworkAddress = new NetworkAddress(settings.Services, IPAddress.Loopback, settings.Port),
-                Nonce = BitConverter.ToUInt64(temp),
+                Nonce = (ulong)rng.NextInt64(),
                 UserAgent = settings.UserAgent,
                 StartHeight = blockchain.Height,
                 Relay = settings.Relay
             };
             return new Message(ver, settings.Network);
         }
+
 
         /// <inheritdoc/>
         public Message[] GetReply(Message msg)
@@ -68,159 +71,168 @@ namespace Autarkysoft.Bitcoin.P2PNetwork
             {
                 // Undefined payload type
                 nodeStatus.AddSmallViolation();
+                nodeStatus.UpdateTime();
+                return null;
             }
-            else
+
+            if (nodeStatus.HandShake != HandShakeState.Finished && plt != PayloadType.Version && plt != PayloadType.Verack)
             {
-                if (nodeStatus.HandShake != HandShakeState.Finished)
-                {
-                    if (plt == PayloadType.Verack)
+                nodeStatus.AddMediumViolation();
+                nodeStatus.UpdateTime();
+                return null;
+            }
+
+            Message[] result = null;
+
+            switch (plt)
+            {
+                case PayloadType.Addr:
+                    break;
+                case PayloadType.Alert:
+                    // Alert messages are ignored
+                    break;
+                case PayloadType.Block:
+                    break;
+                case PayloadType.BlockTxn:
+                    break;
+                case PayloadType.CmpctBlock:
+                    break;
+                case PayloadType.FeeFilter:
+                    break;
+                case PayloadType.FilterAdd:
+                    break;
+                case PayloadType.FilterClear:
+                    break;
+                case PayloadType.FilterLoad:
+                    break;
+                case PayloadType.GetAddr:
+                    break;
+                case PayloadType.GetBlocks:
+                    break;
+                case PayloadType.GetBlockTxn:
+                    break;
+                case PayloadType.GetData:
+                    break;
+                case PayloadType.GetHeaders:
+                    break;
+                case PayloadType.Headers:
+                    break;
+                case PayloadType.Inv:
+                    break;
+                case PayloadType.MemPool:
+                    break;
+                case PayloadType.MerkleBlock:
+                    break;
+                case PayloadType.NotFound:
+                    break;
+                case PayloadType.Ping:
+                    var ping = new PingPayload();
+                    if (!ping.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
                     {
-                        var verack = new VerackPayload();
-                        if (!verack.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
-                        {
-                            nodeStatus.AddSmallViolation();
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.None ||
-                                 nodeStatus.HandShake == HandShakeState.Finished ||
-                                 nodeStatus.HandShake == HandShakeState.SentAndConfirmed)
-                        {
-                            nodeStatus.AddMediumViolation();
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.ReceivedAndReplied ||
-                                 nodeStatus.HandShake == HandShakeState.SentAndReceived)
-                        {
-                            nodeStatus.HandShake = HandShakeState.Finished;
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.Sent)
-                        {
-                            nodeStatus.HandShake = HandShakeState.SentAndConfirmed;
-                        }
+                        nodeStatus.AddSmallViolation();
+                        break;
                     }
-                    else if (plt == PayloadType.Version)
+                    return new Message[1] { new Message(new PongPayload(ping.Nonce), settings.Network) };
+                case PayloadType.Pong:
+                    var pong = new PongPayload();
+                    if (!pong.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
                     {
-                        var version = new VersionPayload();
-                        if (!version.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
-                        {
-                            nodeStatus.AddSmallViolation();
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.None)
-                        {
-                            nodeStatus.HandShake = HandShakeState.ReceivedAndReplied;
-                            return new Message[2]
-                            {
-                                new Message(new VerackPayload(), settings.Network),
-                                GetVersionMsg(version.TransmittingNodeNetworkAddress)
-                            };
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.Sent)
-                        {
-                            nodeStatus.HandShake = HandShakeState.SentAndReceived;
-                            return new Message[1]
-                            {
-                                new Message(new VerackPayload(), settings.Network)
-                            };
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.SentAndConfirmed)
-                        {
-                            nodeStatus.HandShake = HandShakeState.Finished;
-                            return new Message[1]
-                            {
-                                new Message(new VerackPayload(), settings.Network)
-                            };
-                        }
-                        else if (nodeStatus.HandShake == HandShakeState.ReceivedAndReplied ||
-                                 nodeStatus.HandShake == HandShakeState.SentAndReceived ||
-                                 nodeStatus.HandShake == HandShakeState.Finished)
-                        {
-                            nodeStatus.AddMediumViolation();
-                        }
+                        nodeStatus.AddSmallViolation();
+                        break;
                     }
-                    else
-                    {
-                        // HandShake is not complete but the other node is sending other types of messages
-                        nodeStatus.AddMediumViolation();
-                    }
-                }
-                else
-                {
-                    switch (plt)
-                    {
-                        case PayloadType.Addr:
-                            break;
-                        case PayloadType.Alert:
-                            // Alert messages are ignored
-                            break;
-                        case PayloadType.Block:
-                            break;
-                        case PayloadType.BlockTxn:
-                            break;
-                        case PayloadType.CmpctBlock:
-                            break;
-                        case PayloadType.FeeFilter:
-                            break;
-                        case PayloadType.FilterAdd:
-                            break;
-                        case PayloadType.FilterClear:
-                            break;
-                        case PayloadType.FilterLoad:
-                            break;
-                        case PayloadType.GetAddr:
-                            break;
-                        case PayloadType.GetBlocks:
-                            break;
-                        case PayloadType.GetBlockTxn:
-                            break;
-                        case PayloadType.GetData:
-                            break;
-                        case PayloadType.GetHeaders:
-                            break;
-                        case PayloadType.Headers:
-                            break;
-                        case PayloadType.Inv:
-                            break;
-                        case PayloadType.MemPool:
-                            break;
-                        case PayloadType.MerkleBlock:
-                            break;
-                        case PayloadType.NotFound:
-                            break;
-                        case PayloadType.Ping:
-                            var ping = new PingPayload();
-                            if (!ping.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
-                            {
-                                nodeStatus.AddSmallViolation();
-                                break;
-                            }
-                            return new Message[1] { new Message(new PongPayload(ping.Nonce), settings.Network) };
-                        case PayloadType.Pong:
-                            var pong = new PongPayload();
-                            if (!pong.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
-                            {
-                                nodeStatus.AddSmallViolation();
-                                break;
-                            }
-                            break;
-                        case PayloadType.Reject:
-                            // Reject messages are ignored
-                            break;
-                        case PayloadType.SendCmpct:
-                            break;
-                        case PayloadType.SendHeaders:
-                            break;
-                        case PayloadType.Tx:
-                            break;
-                        case PayloadType.Verack:
-                        case PayloadType.Version:
-                            nodeStatus.AddMediumViolation();
-                            break;
-                        default:
-                            break;
-                    }
-                }
+                    break;
+                case PayloadType.Reject:
+                    // Reject messages are ignored
+                    break;
+                case PayloadType.SendCmpct:
+                    break;
+                case PayloadType.SendHeaders:
+                    break;
+                case PayloadType.Tx:
+                    break;
+                case PayloadType.Verack:
+                    CheckVerack();
+                    break;
+                case PayloadType.Version:
+                    result = CheckVersion(msg);
+                    break;
+                default:
+                    break;
             }
 
             nodeStatus.UpdateTime();
-            return null;
+            return result;
+        }
+
+
+        private void CheckVerack()
+        {
+            // VerackPayload doesn't have a body and won't deserialize anything
+            // If anything were added to it in the future a TryDeserialize() should be written here
+
+            switch (nodeStatus.HandShake)
+            {
+                case HandShakeState.None:
+                case HandShakeState.SentAndConfirmed:
+                case HandShakeState.Finished:
+                    nodeStatus.AddMediumViolation();
+                    break;
+                case HandShakeState.ReceivedAndReplied:
+                case HandShakeState.SentAndReceived:
+                    nodeStatus.HandShake = HandShakeState.Finished;
+                    break;
+                case HandShakeState.Sent:
+                    nodeStatus.HandShake = HandShakeState.SentAndConfirmed;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private Message[] CheckVersion(Message msg)
+        {
+            var version = new VersionPayload();
+            if (!version.TryDeserialize(new FastStreamReader(msg.PayloadData), out _))
+            {
+                nodeStatus.AddSmallViolation();
+            }
+
+            Message[] result = null;
+
+            switch (nodeStatus.HandShake)
+            {
+                case HandShakeState.None:
+                    nodeStatus.HandShake = HandShakeState.ReceivedAndReplied;
+                    result = new Message[2]
+                    {
+                        new Message(new VerackPayload(), settings.Network),
+                        GetVersionMsg(version.TransmittingNodeNetworkAddress)
+                    };
+                    break;
+                case HandShakeState.Sent:
+                    nodeStatus.HandShake = HandShakeState.SentAndReceived;
+                    result = new Message[1]
+                    {
+                        new Message(new VerackPayload(), settings.Network)
+                    };
+                    break;
+                case HandShakeState.SentAndConfirmed:
+                    nodeStatus.HandShake = HandShakeState.Finished;
+                    result = new Message[1]
+                    {
+                        new Message(new VerackPayload(), settings.Network)
+                    };
+                    break;
+                case HandShakeState.ReceivedAndReplied:
+                case HandShakeState.SentAndReceived:
+                case HandShakeState.Finished:
+                    nodeStatus.AddMediumViolation();
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
         }
     }
 }
