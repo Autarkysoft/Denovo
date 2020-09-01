0d536e2cf3dffd0eb796562783396490fe99aa83
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-09-01 09:18:16 +0430

    Set curve parameters in constructor rather than each property to avoid repeated calls to BigInt parse method. Credit to u/unit0ne on reddit for pointing this out.

--- Src/Autarkysoft.Bitcoin/Cryptography/Asymmetric/EllipticCurve/SecP256k1.cs
+++ Src/Autarkysoft.Bitcoin/Cryptography/Asymmetric/EllipticCurve/SecP256k1.cs
@@ -15,21 +15,21 @@ namespace Autarkysoft.Bitcoin.Cryptography.Asymmetric.EllipticCurve
         /// <summary>
         /// Prime (FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)
         /// </summary>
-        public BigInteger P => BigInteger.Parse("115792089237316195423570985008687907853269984665640564039457584007908834671663");
+        public BigInteger P { get; } = BigInteger.Parse("115792089237316195423570985008687907853269984665640564039457584007908834671663");
         /// <inheritdoc/>
-        public BigInteger A => BigInteger.Zero;
+        public BigInteger A { get; } = BigInteger.Zero;
         /// <inheritdoc/>
-        public BigInteger B => new BigInteger(7);
+        public BigInteger B { get; } = new BigInteger(7);
         /// <summary>
         /// Curve order (FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141)
         /// </summary>
-        public BigInteger N => BigInteger.Parse("115792089237316195423570985008687907852837564279074904382605163141518161494337");
+        public BigInteger N { get; } = BigInteger.Parse("115792089237316195423570985008687907852837564279074904382605163141518161494337");
         /// <summary>
         /// Curve generator 
         /// <para/> 79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
         /// <para/> 483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
         /// </summary>
-        public EllipticCurvePoint G => new EllipticCurvePoint(
+        public EllipticCurvePoint G { get; } = new EllipticCurvePoint(
             BigInteger.Parse("55066263022277343669578718895168534326250603453777594175500187360389116729240"),
             BigInteger.Parse("32670510020758816978083085130507043184471273380659243275938904335757337482424"));
         /// <inheritdoc/>
