437fe02b1cf51f19c593e19777086bae023f86df
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-10-17 12:33:17 +0330

    Add enum helper

---
+++ Src/Denovo/MVVM/EnumHelper.cs
@@ -0,0 +1,31 @@
+﻿// Denovo
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Denovo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Denovo.MVVM
+{
+    public static class EnumHelper
+    {
+        public static IEnumerable<T> GetAllEnumValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static IEnumerable<DescriptiveItem<T>> GetEnumDescItems<T>(params T[] exclude) where T : Enum
+        {
+            foreach (T item in Enum.GetValues(typeof(T)))
+            {
+                if (exclude != null && !exclude.Contains(item))
+                {
+                    yield return new DescriptiveItem<T>(item);
+                }
+            }
+        }
+    }
+}
