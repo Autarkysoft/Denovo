d1eba78cf852a12965dfd716153eff5a03feb921
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-11-03 08:01:56 +0330

    Add missing license header

--- Src/Denovo/Views/MinerView.xaml.cs
+++ Src/Denovo/Views/MinerView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia;
+﻿// Denovo
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
