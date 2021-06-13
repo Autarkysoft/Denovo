05f6a41db47b4df5030cea3ee2d37880bce48d81
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2021-06-13 17:36:26 +0430

    Update Benchmark.Net and introduce RatioConfig

--- Src/Benchmarks/Bitcoin/Cryptography/Hashing/Sha256Bench.cs
+++ Src/Benchmarks/Bitcoin/Cryptography/Hashing/Sha256Bench.cs
@@ -14,16 +14,17 @@ namespace Benchmarks.Bitcoin.Cryptography.Hashing
     [InProcess]
     [RankColumn]
     [MemoryDiagnoser]
+    [Config(typeof(RatioConfig))]
     public class Sha256Bench
     {
         private readonly SHA256 sysSha = SHA256.Create();
-        private readonly Sha256 libSha = new Sha256();
+        private readonly Sha256 libSha = new();
 
-        public IEnumerable<object[]> GetData()
+        public static IEnumerable<object[]> GetData()
         {
             byte[] small = new byte[33];
             byte[] big = new byte[250];
-            Random rnd = new Random(42);
+            var rnd = new Random(42);
             rnd.NextBytes(small);
             rnd.NextBytes(big);
 
--- Src/Benchmarks/RatioConfig.cs
+++ Src/Benchmarks/RatioConfig.cs
@@ -3,16 +3,17 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
-using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
 
 namespace Benchmarks
 {
-    class Program
+    internal class RatioConfig : ManualConfig
     {
-        static void Main(string[] args)
+        public RatioConfig()
         {
-            //var summary = BenchmarkRunner.Run(typeof(Sha256Bench));
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            SummaryStyle = SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend);
         }
     }
 }
