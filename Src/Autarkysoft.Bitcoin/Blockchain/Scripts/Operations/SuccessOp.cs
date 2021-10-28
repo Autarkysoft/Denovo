70839fc6fe6adb51a02fc83ab3b8f908b9c7801e
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2021-10-28 16:56:16 +0330

    Add new operation: OP_Success + tests

---
+++ Src/Autarkysoft.Bitcoin/Blockchain/Scripts/Operations/SuccessOp.cs
@@ -0,0 +1,25 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+namespace Autarkysoft.Bitcoin.Blockchain.Scripts.Operations
+{
+    /// <summary>
+    /// Operations representing a OP_SuccessX used in Taproot scripts
+    /// </summary>
+    public class SuccessOp : SimpleRunableOpsBase
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="SuccessOp"/> with the given OP value
+        /// </summary>
+        /// <param name="b">OP value (will not be validated)</param>
+        public SuccessOp(byte b)
+        {
+            OpValue = (OP)b;
+        }
+
+        /// <inheritdoc/>
+        public override OP OpValue { get; }
+    }
+}
---
+++ Src/Tests/Bitcoin/Blockchain/Scripts/Operations/SuccessOpTests.cs
@@ -0,0 +1,35 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+using Autarkysoft.Bitcoin.Blockchain.Scripts.Operations;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain.Scripts.Operations
+{
+    public class SuccessOpTests
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            // SuccessOp ctor does not validate OP value (it should be validated by the interpreter)
+            var op1 = new SuccessOp(0);
+            Assert.Equal(OP._0, op1.OpValue);
+
+            var op2 = new SuccessOp(254);
+            Assert.Equal((OP)254, op2.OpValue);
+        }
+
+        [Fact]
+        public void RunTest()
+        {
+            var op = new SuccessOp(254);
+            bool b = op.Run(null, out string error);
+
+            Assert.True(b, error);
+            Assert.Null(error);
+        }
+    }
+}
