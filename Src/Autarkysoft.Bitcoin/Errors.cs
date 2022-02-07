6973b1d0c2200654f6be81d09a66f8460642b7a7
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-02-07 18:16:07 +0330

    Add more error enums

--- Src/Autarkysoft.Bitcoin/Errors.cs
+++ Src/Autarkysoft.Bitcoin/Errors.cs
@@ -3,6 +3,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
+using Autarkysoft.Bitcoin.Blockchain.Scripts;
+
 namespace Autarkysoft.Bitcoin
 {
     /// <summary>
@@ -15,6 +17,18 @@ namespace Autarkysoft.Bitcoin
         /// </summary>
         None,
         /// <summary>
+        /// Invalid or undefined <see cref="NetworkType"/>
+        /// </summary>
+        InvalidNetwork,
+        /// <summary>
+        /// Byte array can not be null
+        /// </summary>
+        NullBytes,
+        /// <summary>
+        /// Byte array can not be null or empty
+        /// </summary>
+        NullOrEmptyBytes,
+        /// <summary>
         /// Null stream
         /// </summary>
         NullStream,
@@ -26,6 +40,66 @@ namespace Autarkysoft.Bitcoin
         /// Data size is bigger than <see cref="int"/>
         /// </summary>
         DataTooBig,
+        /// <summary>
+        /// Invalid DER encoding length.
+        /// </summary>
+        InvalidDerEncodingLength,
+        /// <summary>
+        /// Missing DER sequence tag
+        /// </summary>
+        MissingDerSeqTag,
+        /// <summary>
+        /// Missint first DER int tag
+        /// </summary>
+        MissingDerIntTag1,
+        /// <summary>
+        /// Missint second DER int tag
+        /// </summary>
+        MissingDerIntTag2,
+        /// <summary>
+        /// Invalid DER sequence length
+        /// </summary>
+        InvalidDerSeqLength,
+        /// <summary>
+        /// Invalid DER r length
+        /// </summary>
+        InvalidDerRLength,
+        /// <summary>
+        /// Invalid DER s length
+        /// </summary>
+        InvalidDerSLength,
+        /// <summary>
+        /// Invalid first DER int length
+        /// </summary>
+        InvalidDerIntLength1,
+        /// <summary>
+        /// Invalid second DER int length
+        /// </summary>
+        InvalidDerIntLength2,
+        /// <summary>
+        /// Invalid DER r format
+        /// </summary>
+        InvalidDerRFormat,
+        /// <summary>
+        /// Invalid DER s format
+        /// </summary>
+        InvalidDerSFormat,
+        /// <summary>
+        /// SigHashType byte can not be zero
+        /// </summary>
+        SigHashTypeZero,
+        /// <summary>
+        /// Invalid SigHashType
+        /// </summary>
+        InvalidSigHashType,
+        /// <summary>
+        /// No corresponding output for the input with SigHash_Signle
+        /// </summary>
+        OutOfRangeSigHashSingle,
+        /// <summary>
+        /// Schnorr signature length must be 64 or 65 bytes
+        /// </summary>
+        InvalidSchnorrSigLength,
 
         /// <summary>
         /// Script length bigger than <see cref="Constants.MaxScriptLength"/>
@@ -35,6 +109,34 @@ namespace Autarkysoft.Bitcoin
         /// OP count in a script exceeded the allowed number
         /// </summary>
         OpCountOverflow,
+        /// <summary>
+        /// Stack item count exceeded the allowed number
+        /// </summary>
+        StackItemCountOverflow,
+        /// <summary>
+        /// Item to be pushed to the stack cannot be bigger than <see cref="Constants.MaxScriptItemLength"/>
+        /// </summary>
+        StackPushSizeOverflow,
+        /// <summary>
+        /// There isn't enough items left on the stack
+        /// </summary>
+        NotEnoughStackItems,
+        /// <summary>
+        /// There isn't enough items left on the alt-stack
+        /// </summary>
+        NotEnoughAltStackItems,
+        /// <summary>
+        /// Invalid number format for a stack item
+        /// </summary>
+        InvalidStackNumberFormat,
+        /// <summary>
+        /// Invalid (negative) number on the stack
+        /// </summary>
+        NegativeStackInteger,
+        /// <summary>
+        /// Numbers on the stack are not equal
+        /// </summary>
+        UnequalStackNumbers,
         /// <summary>
         /// Invalid OP code
         /// </summary>
@@ -71,6 +173,46 @@ namespace Autarkysoft.Bitcoin
         /// OP_CheckSigAdd should not be used in legacy and SegWit v0 scripts
         /// </summary>
         OpCheckSigAddPreTaproot,
+        /// <summary>
+        /// A non runable OP was executed
+        /// </summary>
+        NotRunableOp,
+        /// <summary>
+        /// Signature verification failed
+        /// </summary>
+        FailedSignatureVerification,
+        /// <summary>
+        /// Invalid number of public keys in multi-sig script
+        /// </summary>
+        InvalidMultiSigPubkeyCount,
+        /// <summary>
+        /// Invalid number of signatures in multi-sig script
+        /// </summary>
+        InvalidMultiSigSignatureCount,
+        /// <summary>
+        /// Invalid multi-sig dummy item (it has to be OP_0)
+        /// </summary>
+        InvalidMultiSigDummy,
+        /// <summary>
+        /// Too much signature validation relative to witness weight
+        /// </summary>
+        TaprootSigOpOverflow,
+        /// <summary>
+        /// Invalid public key
+        /// </summary>
+        InvalidPublicKey,
+        /// <summary>
+        /// Boolean item popped by conditional OPs must be strict
+        /// </summary>
+        InvalidConditionalBool,
+        /// <summary>
+        /// Top 2 stack items are not equal
+        /// </summary>
+        UnequalStackItems,
+        /// <summary>
+        /// Top stack item is false
+        /// </summary>
+        FalseTopStackItem,
 
         /// <summary>
         /// Stream doesn't start with 0x6a
@@ -135,6 +277,38 @@ namespace Autarkysoft.Bitcoin
         /// </summary>
         UnknownOpPush,
 
+        /// <summary>
+        /// When spending <see cref="OP.CheckLocktimeVerify"/> sequence of all inputs should be less than maximum
+        /// </summary>
+        MaxTxSequence,
+        /// <summary>
+        /// Lock time from stack and transaction are not of the same type
+        /// </summary>
+        UnequalLocktimeType,
+        /// <summary>
+        /// Transaction is unspendable since the locktime is in the future
+        /// </summary>
+        UnspendableLocktime,
+        /// <summary>
+        /// Locktime can not be negative
+        /// </summary>
+        NegativeLocktime,
+        /// <summary>
+        /// Sequence popped from stack can not be negative
+        /// </summary>
+        NegativeSequence,
+        /// <summary>
+        /// Highest bit in input's sequence should be 0
+        /// </summary>
+        InvalidSequenceHighBit,
+        /// <summary>
+        /// Sequence from stack and transaction are not of the same type
+        /// </summary>
+        UnequalSequenceType,
+        /// <summary>
+        /// Transaction version when spending <see cref="OP.CheckSequenceVerify"/> should be bigger than 1
+        /// </summary>
+        InvalidTxVersion,
         /// <summary>
         /// Negative <see cref="Target"/> value
         /// </summary>
