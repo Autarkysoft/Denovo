a31c079bdc1bb99665c39411d470022d274481ec
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-03-28 21:08:57 +0430

    Add 2 new mock classes

---
+++ Src/Tests/Bitcoin/Blockchain/MockBlockVerifier.cs
@@ -0,0 +1,42 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin;
+using Autarkysoft.Bitcoin.Blockchain;
+using Autarkysoft.Bitcoin.Blockchain.Blocks;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain
+{
+    class MockBlockVerifier : IBlockVerifier
+    {
+        internal IBlock expDbBlock;
+        public void UpdateDB(IBlock block)
+        {
+            Assert.Same(expDbBlock, block);
+        }
+
+
+        internal IBlock expVerifyBlock;
+        internal bool verifyResult;
+        public bool Verify(IBlock block, out string error)
+        {
+            Assert.Same(expVerifyBlock, block);
+            error = verifyHeaderResult ? string.Empty : "Foo";
+            return verifyResult;
+        }
+
+
+        internal BlockHeader expHeader;
+        internal Target expTarget;
+        internal bool verifyHeaderResult;
+        public bool VerifyHeader(BlockHeader header, Target expectedTarget)
+        {
+            Assert.Equal(expHeader.GetHash(), header.GetHash());
+            Assert.Equal(expTarget, expectedTarget);
+            return verifyHeaderResult;
+        }
+    }
+}
---
+++ Src/Tests/Bitcoin/Blockchain/MockClientTime.cs
@@ -0,0 +1,25 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.P2PNetwork;
+using System;
+using Xunit;
+
+namespace Tests.Bitcoin.Blockchain
+{
+    class MockClientTime : IClientTime
+    {
+        internal long _now = 123456;
+        public long Now => _now;
+
+        public event EventHandler WrongClockEvent;
+
+        internal long expUptdateTime = -1;
+        public void UpdateTime(long time)
+        {
+            Assert.Equal(expUptdateTime, time);
+        }
+    }
+}
