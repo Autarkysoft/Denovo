77f877cd9b02b2ea3ea8a3d05a80cb422fae21b0
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-11-30 18:32:41 +0330

    Add the "calc" class

---
+++ Src/Autarkysoft.Bitcoin/Cryptography/EllipticCurve/Calc.cs
@@ -0,0 +1,193 @@
+﻿// Autarkysoft.Bitcoin
+// Copyright (c) 2020 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Cryptography.Hashing;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Autarkysoft.Bitcoin.Cryptography.EllipticCurve
+{
+    /// <summary>
+    /// All implementations in this namespace are done with the help of https://github.com/bitcoin-core/secp256k1 
+    /// </summary>
+    public class Calc
+    {
+        public Calc()
+        {
+            // TODO: add more/different contexts here to be selected in ctor using an enum
+            ECMultGenContext();
+        }
+
+        internal PointStorage[,] prec; /* prec[j][i] = 16^j * i * G + U_i */
+
+        public static readonly Point G = new Point(
+            0x16F81798U, 0x59F2815BU, 0x2DCE28D9U, 0x029BFCDBU, 0xCE870B07U, 0x55A06295U, 0xF9DCBBACU, 0x79BE667EU,
+            0xFB10D4B8U, 0x9C47D08FU, 0xA6855419U, 0xFD17B448U, 0x0E1108A8U, 0x5DA4FBFCU, 0x26A3C465U, 0x483ADA77U);
+
+
+        public void ECMultGenContext()
+        {
+            Span<Point> prec = stackalloc Point[1024];
+
+            this.prec = new PointStorage[64, 16];
+            PointJacobian gJ = G.ToPointJacobian();
+            /* Construct a group element with no known corresponding scalar (nothing up my sleeve). */
+            byte[] ba = Encoding.UTF8.GetBytes("The scalar for this x is unknown");
+            Debug.Assert(ba.Length == 32);
+            UInt256_10x26 x = new UInt256_10x26(ba, out bool b);
+            Debug.Assert(b);
+
+            b = Point.TryCreateVar(x, false, out Point nums_ge);
+            Debug.Assert(b);
+
+            PointJacobian numsGJ = nums_ge.ToPointJacobian();
+            /* Add G to make the bits in x uniformly distributed. */
+            numsGJ = numsGJ.AddVar(G, out _);
+
+
+            /* compute prec. */
+            Span<PointJacobian> preJ = stackalloc PointJacobian[1024]; /* Jacobian versions of prec. */
+            PointJacobian gBase = gJ;
+            PointJacobian numsBase = numsGJ;
+            for (int j = 0; j < 64; j++)
+            {
+                /* Set precj[j*16 .. j*16+15] to (numsbase, numsbase + gbase, ..., numsbase + 15*gbase). */
+                preJ[j * 16] = numsBase;
+                for (int i = 1; i < 16; i++)
+                {
+                    preJ[j * 16 + i] = preJ[j * 16 + i - 1].AddVar(gBase, out _);
+                }
+                /* Multiply gbase by 16. */
+                for (int i = 0; i < 4; i++)
+                {
+                    gBase = gBase.DoubleVar(out _);
+                }
+                /* Multiply numbase by 2. */
+                numsBase = numsBase.DoubleVar(out _);
+                if (j == 62)
+                {
+                    /* In the last iteration, numsbase is (1 - 2^j) * nums instead. */
+                    numsBase = numsBase.Negate();
+                    numsBase = numsBase.AddVar(numsGJ, out _);
+                }
+            }
+            Point.SetAllPointsToJacobianVar(prec, preJ, 1024);
+
+            for (int j = 0; j < 64; j++)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    this.prec[j, i] = prec[j * 16 + i].ToStorage();
+                }
+            }
+        }
+
+
+        public PointJacobian MultiplyByG(in Scalar8x32 a)
+        {
+            PointStorage adds = default;
+            PointJacobian result = PointJacobian.Infinity;
+
+            uint[] temp = new uint[] { a.b0, a.b1, a.b2, a.b3, a.b4, a.b5, a.b6, a.b7 };
+            for (int j = 0, k = 0; j < 64;)
+            {
+                uint bit = temp[k] & 0x0000000f;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0x000000f0) >> 4;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0x00000f00) >> 8;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0x0000f000) >> 12;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0x000f0000) >> 16;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0x00f00000) >> 20;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0x0f000000) >> 24;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                bit = (temp[k] & 0xf0000000) >> 28;
+                for (uint i = 0; i < 16; i++)
+                {
+                    adds = PointStorage.CMov(adds, prec[j, i], i == bit ? 1U : 0);
+                }
+                result = result.AddVar(adds.ToPoint(), out _);
+                j++;
+
+                k++;
+            }
+
+            return result;
+        }
+
+
+
+        // This method is a simple way of checking and debugging the code not an actual test
+        public void Test()
+        {
+            using SharpRandom rng = new SharpRandom();
+            byte[] data = new byte[32];
+            rng.GetBytes(data);
+
+            data = new Sha256().ComputeHash(Encoding.UTF8.GetBytes("foo"));
+
+            Scalar8x32 sec = new Scalar8x32(data, out bool overflow);
+            Debug.Assert(!overflow);
+            PointJacobian pj = MultiplyByG(sec);
+            Point p = pj.ToPoint();
+
+            Span<byte> final = p.ToByteArray(false);
+
+            string actual = final.ToArray().ToBase16();
+
+            using Asymmetric.KeyPairs.PrivateKey key = new Asymmetric.KeyPairs.PrivateKey(data);
+            string expected = key.ToPublicKey().ToByteArray(false).ToBase16();
+
+            Debug.Assert(actual == expected);
+        }
+    }
+}
